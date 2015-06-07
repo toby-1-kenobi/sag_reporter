90c84503a2ac80e3ff99909841a2d258e57a4878
Toby Anderson <toby_anderson@sil.org>
2015-06-07 21:36:56 +0530

    Add a test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", phone: "9876543210")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
 end
