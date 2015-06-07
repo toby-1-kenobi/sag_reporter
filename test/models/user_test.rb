b5ef3cb137359e348de5392fe922ea7edb98d97a
Toby Anderson <toby_anderson@sil.org>
2015-06-07 21:53:19 +0530

    Add length validation for user name and phone

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,4 +20,20 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  # In India mobile phone numbers are 10 digits
+  test "phone number should not be too long" do
+    @user.phone = "1" * 11
+    assert_not @user.valid?
+  end
+
+  test "phone number should not be too short" do
+    @user.phone = "1" * 9
+    assert_not @user.valid?
+  end
+
 end
