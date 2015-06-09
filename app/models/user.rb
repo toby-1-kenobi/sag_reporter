0c7e60573374cdeb72c98190a821538c6ecc2a8f
Toby Anderson <toby_anderson@sil.org>
2015-06-09 14:13:54 +0530

    Add validation for password presence and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :phone, presence: true, length: { is: 10 }, format: { with: /\A\d+\Z/ }, uniqueness: true
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
