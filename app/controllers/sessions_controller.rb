8d51f3c24b4490186ac9fd6869bb3c1b98e44124
Toby Anderson <toby_anderson@sil.org>
2015-06-13 22:41:20 +0530

    Put in some authenticating code to the sessions controller for login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(phone: params[:session][:phone])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash["error"] = 'Phone number or password not correct' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
