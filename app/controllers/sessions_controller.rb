69726cf94202aa746c0ca4f73e1f9ebc766b011e
Toby Anderson <toby_anderson@sil.org>
2015-06-13 22:47:20 +0530

    Fix bug that makes login flash persist too long, and add a test for that.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash["error"] = 'Phone number or password not correct' # Not quite right!
+      flash.now["error"] = 'Phone number or password not correct' # Not quite right!
       render 'new'
     end
   end
