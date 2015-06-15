c4ecdf516939b6c6b7d554210076397cdb77b45d
Toby Anderson <toby_anderson@sil.org>
2015-06-15 16:16:45 +0530

    Add home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  root 'users#new'
+
+  root 'static_pages#home'
 
   resources :users
 
   get 'adduser' => 'users#new'
+  get 'home' => 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
