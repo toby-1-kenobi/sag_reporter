53de1c18307b9377d270beb643a3242a40722204
Toby Anderson <toby_anderson@sil.org>
2015-07-04 12:41:28 +0530

    Seed the database with some permissions, and admin role and an admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             phone: "1234567890",
-             password:              "foobar",
-             password_confirmation: "foobar")
 
-99.times do |n|
-  name  = Faker::Name.name
-  phone = Faker::Number.number(10)
-  password = "password"
-  User.create!(name:  name,
-               phone: phone,
-               password:              password,
-               password_confirmation: password)
-end
+Permission.create(name: "create_user", description: "Create a new user")
+Permission.create(name: "edit_user", description: "Edit any user details")
+Permission.create(name: "view_all_users", description: "View all users")
+Permission.create(name: "delete_user", description: "Delete any other user")
+Permission.create(name: "view_roles", description: "View all roles with permissions")
+Permission.create(name: "edit_role", description: "Edit any role with its permissions")
+Permission.create(name: "create_role", description: "Create a new role")
+
+admin_role = Role.create(name: "admin")
+admin_role.permissions = Permission.all
+
+User.create!(name:  "admin",
+             phone: "1234567890",
+             password:              "password",
+             password_confirmation: "password",
+             role: admin_role)
+
+#99.times do |n|
+#  name  = Faker::Name.name
+#  phone = Faker::Number.number(10)
+#  password = "password"
+#  User.create!(name:  name,
+#               phone: phone,
+#               password:              password,
+#               password_confirmation: password)
+#end
