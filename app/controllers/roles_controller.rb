847673512ba50ecc009101758e76b38a9e84b7b7
Toby Anderson <toby_anderson@sil.org>
2015-07-07 15:51:27 +0530

    Update role permissions according to submitted data and redirect to roles index with a success flash.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -6,8 +6,20 @@ class RolesController < ApplicationController
   end
 
   def update
-  	# insert code to update roles.
-  	redirect_to roles_url
+  	# parameters come as a hash with role names as keys
+  	# and a hash of permissions as values. Each key in the permissions hash
+  	# is a permission that should be in that role. No other permission should be.
+  	# So for each role clear the permissions, then add permissions
+  	# from the keys of the inner hash.
+  	params['roles'].each_pair do |role_name, permissions|
+  	  role = Role.find_by_name(role_name)
+  	  role.permissions.clear
+  	  permissions.each_key do |perm_name|
+  	  	role.permissions << Permission.find_by_name(perm_name)
+  	  end
+  	end
+  	flash["success"] = 'Roles updated.'
+  	redirect_to roles_url	
   end
 
   def create
