4f41fdb6345dfbd207a073a454025ba75dae9b79
Toby Anderson <toby_anderson@sil.org>
2015-07-31 23:34:25 +0530

    Seperate fixtures into those used to seed the database and those only used in testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#Permission.create(name: "create_user", description: "Create a new user")
-#Permission.create(name: "edit_user", description: "Edit any user details")
-#Permission.create(name: "view_all_users", description: "View all users")
-#Permission.create(name: "delete_user", description: "Delete any other user")
-#Permission.create(name: "view_roles", description: "View all roles with permissions")
-#Permission.create(name: "edit_role", description: "Edit any role with its permissions")
-#Permission.create(name: "create_role", description: "Create a new role")
+require 'active_record/fixtures'
+
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "languages")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "permissions")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "roles")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "topics")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "users")
 
 #admin_role = Role.create(name: "admin")
 #admin_role.permissions = Permission.all
@@ -22,13 +22,3 @@
 #             password:              "password",
 #             password_confirmation: "password",
 #             role: admin_role)
-
-#99.times do |n|
-#  name  = Faker::Name.name
-#  phone = Faker::Number.number(10)
-#  password = "password"
-#  User.create!(name:  name,
-#               phone: phone,
-#               password:              password,
-#               password_confirmation: password)
-#end
