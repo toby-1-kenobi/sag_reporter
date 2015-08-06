8861608582623b25502010c7a3172769f2b1d095
Toby Anderson <toby_anderson@sil.org>
2015-08-06 14:22:16 +0530

    Add a refernece to the record creator for person records.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,8 @@ class Person < ActiveRecord::Base
 
   include ContactDetails
 
-  belongs_to :language
+  belongs_to :mother_tongue, class_name: "Language", foreign_key: "language_id"
+  belongs_to :record_creator, class_name: "User", foreign_key: "user_id"
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :phone, length: { is: 10 }, allow_nil: true, numericality: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806030053) do
+ActiveRecord::Schema.define(version: 20150806081203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,9 +74,11 @@ ActiveRecord::Schema.define(version: 20150806030053) do
     t.integer  "language_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   add_index "people", ["language_id"], name: "index_people_on_language_id", using: :btree
+  add_index "people", ["user_id"], name: "index_people_on_user_id", using: :btree
 
   create_table "permissions", force: :cascade do |t|
     t.string   "name"
@@ -168,6 +170,7 @@ ActiveRecord::Schema.define(version: 20150806030053) do
   add_foreign_key "languages_tallies", "languages"
   add_foreign_key "languages_tallies", "tallies"
   add_foreign_key "people", "languages"
+  add_foreign_key "people", "users"
   add_foreign_key "tallies", "topics"
   add_foreign_key "tally_updates", "languages_tallies"
   add_foreign_key "tally_updates", "users"
