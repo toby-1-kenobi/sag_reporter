7a293c022be9d42527ae913f0b67f3d4c5057957
Toby Anderson <toby_anderson@sil.org>
2015-08-06 10:49:24 +0530

    Refactor phone fixing method to be in a concern included by user and person model.

---
+++ app/models/concerns/contact_details.rb
@@ -0,0 +1,34 @@
+module ContactDetails
+
+  extend ActiveSupport::Concern
+
+  included do
+
+  	before_validation :fix_phone
+
+    # Pretty-print phone number
+    def pretty_phone
+      self.phone.slice(0..3) + ' ' + self.phone.slice(4..6) + ' ' + self.phone.slice(7..-1)
+    end
+
+  end
+
+  private
+
+    def fix_phone
+      if self.phone
+        # remove non digits from the phone number
+        self.phone.gsub!(/[^0-9]/, '')
+        # if it starts with "91" and is longer than 11 digits
+        # then it's got a prefix we need to remove
+        if self.phone.length > 11 and self.phone.start_with?("91")
+          self.phone = self.phone.slice(2..-1)
+        end
+        # another prefix possibility is "0"
+        if self.phone.length > 10 and self.phone.start_with?("0")
+          self.phone = self.phone.slice(1..-1)
+        end
+      end
+    end
+
+end
