ee489503d739474d1aa5008cb3d0a5cf7152767a
Toby Anderson <toby_anderson@sil.org>
2015-08-07 13:55:56 +0530

    Group permissions by category.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,5 +1,7 @@
 class Permission < ActiveRecord::Base
 
+	enum category: [ :users, :roles, :languages, :topics, :reports, :tallies, :events, :people ]
+
 	has_and_belongs_to_many :roles
 	validates :name, presence: true, allow_nil: false, uniqueness: true
 	
