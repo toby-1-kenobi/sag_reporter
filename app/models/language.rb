5bf02a61ac44d567adf7758cdeae29a5b63984bd
Toby Anderson <toby_anderson@sil.org>
2015-08-10 10:51:57 +0530

    fix spelling error

--- app/models/language.rb
+++ app/models/language.rb
@@ -3,7 +3,7 @@ class Language < ActiveRecord::Base
   has_many :user_mt_speakers, class_name: 'User', foreign_key: 'mother_tongue_id'
   has_and_belongs_to_many :user_speakers, class_name: 'User'
   has_and_belongs_to_many :reports
-  has_many :language_tallies, class_name: 'LanguagesTally', dependant: :destroy
+  has_many :language_tallies, class_name: 'LanguagesTally', dependent: :destroy
   has_many :tallies, through: :language_tallies
 
   validates :name, presence: true, allow_nil: false, uniqueness: true
