d4fe7b4c6eca626b34599711bdf343f8a23a1228
Toby Anderson <toby_anderson@sil.org>
2015-08-10 10:51:57 +0530

    fix spelling error

--- app/models/tally.rb
+++ app/models/tally.rb
@@ -3,7 +3,7 @@ class Tally < ActiveRecord::Base
 	enum state: [ :archived, :active ]
 
 	belongs_to :topic
-	has_many :language_tallies, class_name: 'LanguagesTally', dependant: :destroy
+	has_many :language_tallies, class_name: 'LanguagesTally', dependent: :destroy
 	has_many :languages, through: :language_tallies
 	has_many :tally_updates, through: :language_tallies
 	
