c0f8fc69a35e7e0085e2d1e6fbadca147aca098e
Toby Anderson <toby_anderson@sil.org>
2015-08-11 12:07:34 +0530

    Link events to people and languages in the database. Use Attendance model to link people and events.

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,8 @@ class Person < ActiveRecord::Base
 
   belongs_to :mother_tongue, class_name: "Language", foreign_key: "language_id"
   belongs_to :record_creator, class_name: "User", foreign_key: "user_id"
+  has_many :attendances, dependent: :destroy
+  has_many :events, through: :attendances
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :phone, length: { is: 10 }, allow_nil: true, numericality: true
