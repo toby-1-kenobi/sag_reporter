bcd63c896b6fa230231c3d5a0527bd59ec9261cf
Toby Anderson <toby_anderson@sil.org>
2015-08-12 00:48:48 +0530

    Add ActionPoint model and enable event report form to generate ActionPooints. Also fix Field report form to take one of two types of report - event or planning.

--- app/models/action_point.rb
+++ app/models/action_point.rb
@@ -1,4 +1,7 @@
 class ActionPoint < ActiveRecord::Base
+
+  enum status: [ :incomplete, :complete ]
+
   belongs_to :responsible, class_name: 'Person'
   belongs_to :record_creator, class_name: 'User'
   belongs_to :event
