ef609da5c81e34c602fbabb7f31cf4791cdf6a6d
Toby Anderson <toby_anderson@sil.org>
2015-08-11 22:17:21 +0530

    Enable showing an event reported on.

--- app/models/impact_report.rb
+++ app/models/impact_report.rb
@@ -5,6 +5,7 @@ class ImpactReport < ActiveRecord::Base
   belongs_to :reporter, class_name: 'User'
   belongs_to :event
   belongs_to :progress_marker
+  delegate :topic, to: :progress_marker, :allow_nil => true
   has_and_belongs_to_many :languages
 
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -9,4 +9,13 @@ class Report < ActiveRecord::Base
 	validates :content, presence: true, allow_nil: false
 	validates :reporter, presence: true, allow_nil: false
 
+  def self.categories
+    {
+      'mt_society' => "use of mother tongue in society",
+      'mt_church' => "use of mother tongue tools in the local churches",
+      'needs_society' => "needs of society",
+      'needs_church' => "needs of the churches"
+    }
+  end
+
 end
