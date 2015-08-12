fea6fb5fd957d17c5223c0d695c904c359ec7a21
Toby Anderson <toby_anderson@sil.org>
2015-08-12 23:37:50 +0530

    Enable taggin of impact reports with progress markers through ajax.

--- app/controllers/impact_reports_controller.rb
+++ app/controllers/impact_reports_controller.rb
@@ -1,7 +1,26 @@
 class ImpactReportsController < ApplicationController
 
+  before_action :require_login
+
   def show
   	@report = ImpactReport.find(params[:id])
+  end
+
+  def tag
+  	@reports = ImpactReport.where(progress_marker: nil)
+  	@outcome_areas = Topic.all
+  	@progress_markers_by_oa = ProgressMarker.all.group_by{ |pm| pm.topic }
+  end
+
+  def tag_update
+  	report = ImpactReport.find(params[:id])
+  	progress_marker = ProgressMarker.find(params[:pm_id])
+  	report.progress_marker = progress_marker
+  	if report.save
+  	  render text: "success report: #{params[:id]} pm: #{params[:pm_id]}"
+  	else
+  	  render text: "fail report: #{params[:id]} pm: #{params[:pm_id]}"
+  	end
   end
 
 end
