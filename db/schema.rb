8d9f0c6cb0a6f0c5a64b59bf367f7aba05095dfc
Toby Anderson <toby_anderson@sil.org>
2015-08-17 22:25:15 +0530

    Progress markers remember last progress.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817055650) do
+ActiveRecord::Schema.define(version: 20150817113439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 20150817055650) do
     t.datetime "updated_at",           null: false
   end
 
+  add_index "progress_updates", ["created_at"], name: "index_progress_updates_on_created_at", using: :btree
   add_index "progress_updates", ["language_progress_id"], name: "index_progress_updates_on_language_progress_id", using: :btree
   add_index "progress_updates", ["user_id"], name: "index_progress_updates_on_user_id", using: :btree
 
