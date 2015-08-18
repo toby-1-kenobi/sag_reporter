8c0aab01f503fea94ee8a319dd848e39d9069837
Toby Anderson <toby_anderson@sil.org>
2015-08-18 17:12:13 +0530

    version update to 0.5

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,7 +6,7 @@ module EventsHelper
 			'mt_church' => "Was anything said about <strong>use of mother tongue tools in the local churches</strong>?".html_safe,
 			'need_society' => "Was anything said about <strong>needs of society</strong>?".html_safe,
 			'need_church' => "Was anything said about <strong>needs of the churches</strong>?".html_safe,
-			'plan' => "Were any other <strong>hopes, dreams or challenges?</strong> shared?".html_safe,
+			'plan' => "Were any other <strong>hopes, dreams or challenges</strong> shared?".html_safe,
 			'impact' => "Were any other <strong>impact stories</strong> shared?".html_safe
 		}
 	end
