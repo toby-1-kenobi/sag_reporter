db428f024d5fb24c815880207062796bcf2d5a17
Toby Anderson <toby_anderson@sil.org>
2015-08-28 00:10:48 +0530

    Bug fix for submitting reports.

--- app/models/concerns/state_based.rb
+++ app/models/concerns/state_based.rb
@@ -10,9 +10,9 @@ module StateBased
   private
 
   def geo_state_init
-    self.geo_state ||= record_creator.geo_state if respond_to? "record_creator"
-    self.geo_state ||= reporter.geo_state if respond_to? "reporter"
-    self.geo_state ||= user.geo_state if respond_to? "user"
+    self.geo_state ||= record_creator.geo_state if respond_to? "record_creator" and record_creator
+    self.geo_state ||= reporter.geo_state if respond_to? "reporter" and reporter
+    self.geo_state ||= user.geo_state if respond_to? "user" and user
   end
 
 end
