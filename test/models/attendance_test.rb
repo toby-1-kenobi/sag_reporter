76217e74fab182e3e84fc27fd4d0a1e907af6e42
Toby Anderson <toby_anderson@sil.org>
2015-08-28 13:50:27 +0530

    Add validity constraints to Attendance.

--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe Attendance do
-  let(:attendance) { Attendance.new }
+
+  let (:john) { Person.new name: "John" }
+  let (:my_event) { Event.new event_label: "label", event_date: Date.today }
+  let(:attendance) { Attendance.new person: john, event: my_event}
 
   it "must be valid" do
     value(attendance).must_be :valid?
   end
+
+  it "wont be valid without a person" do
+    attendance.person = nil
+    value(attendance).wont_be :valid?
+  end
+
+  it "wont be valid without an event" do
+    attendance.event = nil
+    value(attendance).wont_be :valid?
+  end
+
+  it "must be unique" do
+    attendance2 = attendance.dup
+    attendance.save
+    attendance2.wont_be :valid?
+  end
+
 end
