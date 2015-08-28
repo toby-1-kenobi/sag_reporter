25f93005982bd2479028f02b17f8f07433af540b
Toby Anderson <toby_anderson@sil.org>
2015-08-28 13:10:10 +0530

    Add testing and functionality on ActionPoint model.

--- app/models/action_point.rb
+++ app/models/action_point.rb
@@ -5,5 +5,12 @@ class ActionPoint < ActiveRecord::Base
   belongs_to :responsible, class_name: 'Person'
   belongs_to :record_creator, class_name: 'User'
   belongs_to :event
+
+  validates :content, presence: true
+  validates :responsible, presence: true
+
+  def mark_completed
+    self.status = :complete
+  end
   
 end
--- test/models/action_point_test.rb
+++ test/models/action_point_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe ActionPoint do
-  let(:action_point) { ActionPoint.new }
+
+  let(:john) { Person.new name: "John" }
+  let(:action_point) { ActionPoint.new responsible: john, content: "do something" }
 
   it "must be valid" do
     value(action_point).must_be :valid?
   end
+
+  it "wont be valid without content" do
+    action_point.content = nil
+    value(action_point).wont_be :valid?
+    action_point.content = ""
+    value(action_point).wont_be :valid?
+  end
+
+  it "wont be valid without a responsible person" do
+    action_point.responsible = nil
+    value(action_point).wont_be :valid?
+  end
+
+  it "must be incomplete until marked completed" do
+    value(action_point).must_be :incomplete?
+    action_point.mark_completed
+    value(action_point).must_be :complete?
+  end
+
 end
