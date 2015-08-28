0c7526cd1d2d6b9b05eef2efaa6e04002af91da5
Toby Anderson <toby_anderson@sil.org>
2015-08-28 15:33:25 +0530

    Add validation contratins to Creation model.

--- app/models/creation.rb
+++ app/models/creation.rb
@@ -1,4 +1,10 @@
 class Creation < ActiveRecord::Base
+
   belongs_to :person
   belongs_to :mt_resource
+
+  validates :person, presence: true
+  validates :mt_resource, presence: true
+  #TODO: add null constraints to db table
+
 end
--- test/models/creation_test.rb
+++ test/models/creation_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 describe Creation do
-  let(:creation) { Creation.new }
+
+  let(:john) { Person.new name: "John" }
+  let(:resource) { MtResource.new }
+  let(:creation) { Creation.new person: john, mt_resource: resource }
 
   it "must be valid" do
     value(creation).must_be :valid?
   end
+
+  it "wont be valid without a person" do
+    creation.person = nil
+    value(creation).wont_be :valid?
+  end
+
+  it "wont be value without a resource" do
+    creation.mt_resource = nil
+    value(creation).wont_be :valid?
+  end
+  
+
 end
