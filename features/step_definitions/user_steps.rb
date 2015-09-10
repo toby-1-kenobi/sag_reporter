3306bc1bad40d585f5b9edf6814f76074cc0ea6b
Toby Anderson <toby_anderson@sil.org>
2015-09-10 12:11:02 +0700

    Add cucumber feature file users having multiple states.

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,49 @@
+
+
+When(/^I visit the new user page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the state selector has no states$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^there is a zone selector$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the zone north_east$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the state selector (has|does not have) the states (.*)$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the zone hindi_zone$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I unselect the zone north_east$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the states bihar and up$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I complete the user form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I submit the form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am on the user page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see bihar and up$/) do
+  pending # express the regexp above with the code you wish you had
+end
