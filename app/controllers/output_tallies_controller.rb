79207f1f67d0ebfed185e40c94cdc22e55ebee7a
Toby Anderson <toby.anderson@gmail.com>
2015-11-03 23:19:39 +1100

    OutputCounts validate on their language being a member of their geo_state. If the user attempts to make invalid OutputCounts in the report numbers form then they are redirected back to the form with helpful error messages.

--- app/controllers/output_tallies_controller.rb
+++ app/controllers/output_tallies_controller.rb
@@ -13,23 +13,36 @@ class OutputTalliesController < ApplicationController
   	year = Date.today.year
   	if month > Date.today.month then year = year - 1 end
 
-    geo_state = GeoState.find(params['outputs']['geo_state'])
+    geo_state = GeoState.find(params['outputs']['geo_state_id'])
+
+    @failedCounts = Array.new
 
   	params['amounts'].select{ |k,v| v and params[k] and not params[k].empty? }.each do |code, amount|
   	  language = Language.find(params[code])
   	  tally = OutputTally.find(code.split('__').first)
-  	  OutputCount.create(
-  	  	user: current_user,
+      outputCountParams = {
+        user: current_user,
         geo_state: geo_state,
-  	  	output_tally: tally,
-  	  	language: language,
-  	  	amount: amount,
-  	  	year: year,
-  	  	month: month
-  	  )
+        output_tally: tally,
+        language: language,
+        amount: amount,
+        year: year,
+        month: month
+      }
+  	  outputCount = OutputCount.new(outputCountParams)
+      if !outputCount.save
+        @failedCounts << outputCount
+      end
   	end
-  	flash.now['success'] = "Numbers recorded"
-  	render 'static_pages/home'
+    if @failedCounts.length == 0
+    	flash.now['success'] = "Numbers recorded"
+    	render 'static_pages/home'
+    else
+      flash.now['error'] = "Some numbers could not be recorded"
+      @output_tallies_by_topic = OutputTally.all.group_by{ |ot| ot.topic }
+      @languages = Language.minorities(current_user.geo_states)
+      render 'report_numbers'
+    end
 
   end
 
--- app/models/output_count.rb
+++ app/models/output_count.rb
@@ -7,12 +7,19 @@ class OutputCount < ActiveRecord::Base
   belongs_to :language
 
   before_validation :date_init
+  validate :language_must_be_in_state
 
   private
 
   def date_init
   	self.year ||= Date.today.year if new_record?
   	self.month ||= Date.today.month if new_record?
+  end
+
+  def language_must_be_in_state
+    if !geo_state.languages.include? language
+      errors.add(:language, "must be in the state selected")
+    end
   end
 
 end
