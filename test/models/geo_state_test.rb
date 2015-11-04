e1d598a3825313e02c83f7cc368fdd23ec7b7edf
Toby Anderson <toby.anderson@gmail.com>
2015-11-04 12:34:02 +1100

    Fix progress marker updating to work better with users having multiple states.

--- test/models/geo_state_test.rb
+++ test/models/geo_state_test.rb
@@ -4,6 +4,8 @@ describe GeoState do
 
   let(:north_east_zone) { Zone.new name: "North East"}
   let(:assam_state) { GeoState.new name: "Assam", zone: north_east_zone }
+  let(:minority_language) { Language.new name: "Toto", lwc: false }
+  let(:state_language) { Language.new name: "Assamese", lwc: true }
 
   it "must be valid" do
     value(assam_state).must_be :valid?
@@ -13,6 +15,13 @@ describe GeoState do
   	north_east_zone.stub(:id, 8) do
   	  value(assam_state.zone_id).must_equal north_east_zone.id
   	end
+  end
+
+  it "returns its minority languages" do
+    assam_state.languages << minority_language
+    assam_state.languages << state_language
+    assam_state.minority_languages.must_include minority_language
+    assam_state.minority_languages.wont_include state_language
   end
   
 end
