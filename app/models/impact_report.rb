10ba2f2b46c179f8ab4e81a7c21142a1a77e1274
Toby Anderson <toby.anderson@gmail.com>
2015-11-17 11:14:00 +1100

    Reports must have a state (active/archived) and new reports are active unless otherwise specified.

--- app/models/impact_report.rb
+++ app/models/impact_report.rb
@@ -7,8 +7,12 @@ class ImpactReport < ActiveRecord::Base
   belongs_to :reporter, class_name: 'User'
   belongs_to :event
   belongs_to :progress_marker
-  delegate :topic, to: :progress_marker, :allow_nil => true
-  has_and_belongs_to_many :languages
+  delegate :topic, to: :progress_marker, allow_nil: true
+  has_and_belongs_to_many :languages, allow_nil: false
+
+  validates :state, presence: true
+
+  after_initialize :state_init
 
   def report_date
   	event ? event.event_date : created_at
@@ -17,5 +21,11 @@ class ImpactReport < ActiveRecord::Base
   def report_type
     "impact"
   end
+
+  private
+
+    def state_init
+      self.state ||= :active
+    end
 
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 class SagReporter::Application
-  VERSION = '0.11.3'
+  VERSION = '0.11.4'
 end
