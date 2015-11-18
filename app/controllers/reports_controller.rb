336e439c230eb63505a49406fadc7a729b1832c3
Toby Anderson <toby.anderson@gmail.com>
2015-11-18 11:02:51 +1100

    Languages in the selection dropdown reflect selected state in edit and add forms for reports and events.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -53,33 +53,34 @@ class ReportsController < ApplicationController
       flash["success"] = "New Report Submitted!"
       redirect_to @report
     else
-  	  @minority_languages = Language.minorities(current_user.geo_states).order("LOWER(languages.name)")
+      @minority_languages = Language.minorities(current_user.geo_states).order("LOWER(languages.name)")
   	  @topics = Topic.all
       render 'new'
     end
   end
 
   def show
-  	@report = Report.find(params[:id])
+    @report = Report.find(params[:id])
   end
 
   def edit
-  	@report = Report.find(params[:id])
-  	@minority_languages = Language.minorities(current_user.geo_states)
-  	@topics = Topic.all
+    @report = Report.find(params[:id])
+    @geo_states = @report.available_geo_states(current_user)
+    @minority_languages = Language.minorities(@geo_states)
+    @topics = Topic.all
   end
 
   def update
   	@report = Report.find(params[:id])
   	if @report.update_attributes(report_params)
       if params['report']['languages']
-      	@report.languages.clear
+        @report.languages.clear
         params['report']['languages'].each do |lang_id, value|
           @report.languages << Language.find_by_id(lang_id.to_i)
         end
       end
       if params['report']['topics']
-      	@report.topics.clear
+        @report.topics.clear
         params['report']['topics'].each do |top_id, value|
           @report.topics << Topic.find_by_id(top_id.to_i)
         end
@@ -90,41 +91,41 @@ class ReportsController < ApplicationController
   end
 
   def index
-  	@reports = Report.where(geo_state: current_user.geo_state).order(:created_at => :desc).paginate(page: params[:page])
+    @reports = Report.where(geo_state: current_user.geo_state).order(:created_at => :desc).paginate(page: params[:page])
     @impact_reports = ImpactReport.where(geo_state: current_user.geo_state).order(:created_at => :desc).paginate(page: params[:page])
-  	recent_view
+    recent_view
   end
 
   def by_language
-  	@reports = Report.where(geo_state: current_user.geo_state).order(:created_at => :desc)
+    @reports = Report.where(geo_state: current_user.geo_state).order(:created_at => :desc)
     @impact_reports = ImpactReport.where(geo_state: current_user.geo_state).order(:created_at => :desc)
-  	@languages = Language.all.order("LOWER(languages.name)")
-  	recent_view
+    @languages = Language.all.order("LOWER(languages.name)")
+    recent_view
   end
 
   def by_topic
-  	@reports = Report.where(geo_state: current_user.geo_state).all.order(:created_at => :desc)
+    @reports = Report.where(geo_state: current_user.geo_state).all.order(:created_at => :desc)
     @impact_reports = ImpactReport.where(geo_state: current_user.geo_state).order(:created_at => :desc)
-  	@topics = Topic.all
-  	recent_view
+    @topics = Topic.all
+    recent_view
   end
 
   def by_reporter
-  	@reports = Report.where(geo_state: current_user.geo_state).all.order(:created_at => :desc)
+    @reports = Report.where(geo_state: current_user.geo_state).all.order(:created_at => :desc)
     @impact_reports = ImpactReport.where(geo_state: current_user.geo_state).order(:created_at => :desc)
-  	recent_view
+    recent_view
   end
 
   def archive
-  	report = Report.find(params[:id])
+    report = Report.find(params[:id])
     report.archived!
-  	redirect_recent_or root_path
+    redirect_recent_or root_path
   end
 
   def unarchive
-  	report = Report.find(params[:id])
+    report = Report.find(params[:id])
     report.active!
-  	redirect_recent_or report
+    redirect_recent_or report
   end
 
     private
@@ -133,7 +134,7 @@ class ReportsController < ApplicationController
       if current_user.can_archive_report?
         permitted = params.require(:report).permit(:content, :mt_society, :mt_church, :needs_society, :needs_church, :geo_state_id, :state)
       else
-    	  permitted = params.require(:report).permit(:content, :mt_society, :mt_church, :needs_society, :needs_church, :geo_state_id)
+        permitted = params.require(:report).permit(:content, :mt_society, :mt_church, :needs_society, :needs_church, :geo_state_id)
       end
     end
   
