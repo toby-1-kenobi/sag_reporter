efd99ca2bbae1b04181c3fd5909090f1d0b13827
Toby Anderson <toby.anderson@gmail.com>
2015-11-26 10:12:58 +1100

    Users must have at least one geo_state to be valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :mother_tongue_id, presence: true, allow_nil: false
   validates :role_id, presence: true, allow_nil: false
+  validates :geo_states, presence: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
