06d848b99098d1318c4d0a1094ca42ea52f41534
Toby Anderson <toby.anderson@gmail.com>
2015-12-10 11:40:37 +1100

    Make sure, when there is more than one progress update for the same month, language and PM, that the latest one added is used in language progress.

--- app/models/language_progress.rb
+++ app/models/language_progress.rb
@@ -16,18 +16,22 @@ class LanguageProgress < ActiveRecord::Base
 
   def current_value(geo_state = nil)
     if geo_state
-      progress_updates.where(geo_state: geo_state).empty? ? 0 : progress_updates.where(geo_state: geo_state).order("year, month").last.progress
+      progress_updates.where(geo_state: geo_state).empty? ? 0 : progress_updates.where(geo_state: geo_state).order("year, month, created_at").last.progress
     else
       # This gives you the latest update across the whole language
       # I don't think it's very useful since all updates are by state
-    	progress_updates.empty? ? 0 : progress_updates.order("created_at").last.progress
+    	progress_updates.empty? ? 0 : progress_updates.order("year, month, created_at").last.progress
     end
   end
 
+  # find the latest progress update in or before a given month
+  # and use this to get the score for that month
   def month_score(geo_state, year = Date.today.year, month = Date.today.month)
   	cutoff = Date.new(year, month, -1).end_of_day
     state_updates = progress_updates.where(geo_state: geo_state).select{ |pu| pu.progress_date <= cutoff }
-  	return state_updates.empty? ? 0 : state_updates.max_by(&:progress_date).progress * progress_marker.weight
+    # if more than one update shares the same progress_date then max_by will select the first of these
+    # we want the one added most recently so we reverse sort by created_at
+  	return state_updates.empty? ? 0 : state_updates.sort{ |a,b| b.created_at <=> a.created_at }.max_by(&:progress_date).progress * progress_marker.weight
   end
 
 end
