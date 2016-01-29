af2e752e782f33e8d366255497505af8c68bbe19
Toby Anderson <toby.anderson@gmail.com>
2016-01-29 11:25:35 +1100

    Add validations to District and SubDistrict so that their names are unique within their own regions.

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,6 +2,6 @@ class District < ActiveRecord::Base
   belongs_to :geo_state
   has_many :sub_districts, dependent: :destroy
 
-  validates :name, presence: true, allow_nil: false
+  validates :name, presence: true, allow_nil: false, uniqueness: { scope: :geo_state }
   validates :geo_state, presence: true, allow_nil: false
 end
