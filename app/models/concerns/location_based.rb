bb0be2c2f8a83c60d3cec4fae629597f75c13664
Toby Anderson <toby.anderson@gmail.com>
2016-02-02 15:35:19 +1100

    Add the location_based module for things that have a sub_district.

---
+++ app/models/concerns/location_based.rb
@@ -0,0 +1,23 @@
+module LocationBased
+
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :sub_district
+    validates :sub_district, presence: 
+    validate :location_must_be_in_geo_state
+  end
+
+  private
+
+  def geo_state_init
+    self.geo_state ||= sub_district.geo_state
+    super
+  end
+
+  def location_must_be_in_geo_state
+    if sub_district.geo_state != geo_state
+      errors.add(:sub_district, "must be located within the State")
+  end
+
+end
--- app/models/sub_district.rb
+++ app/models/sub_district.rb
@@ -1,6 +1,8 @@
 class SubDistrict < ActiveRecord::Base
   belongs_to :district
 
+  delegates :geo_state, to: :district
+
   validates :name, presence: true, allow_nil: false, uniqueness: { scope: :district }
   validates :district, presence: true, allow_nil: false
 end
