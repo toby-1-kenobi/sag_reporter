bb7f8e305f2084c705202d7f45d6fa9d24219661
Toby Anderson <toby.anderson@gmail.com>
2016-02-03 13:30:50 +1100

    Change the migration on events to only add the reference to sub_district and (rename depricated columns) at this stage because of complications in migrating existing data to references. Add the relevant code to the Event model.

--- app/models/concerns/location_based.rb
+++ app/models/concerns/location_based.rb
@@ -4,6 +4,7 @@ module LocationBased
 
   included do
     belongs_to :sub_district
+    delegate :district, to: :sub_district
     validates :sub_district, presence: 
     validate :location_must_be_in_geo_state
   end
@@ -18,6 +19,7 @@ module LocationBased
   def location_must_be_in_geo_state
     if sub_district.geo_state != geo_state
       errors.add(:sub_district, "must be located within the State")
+    end
   end
 
 end
