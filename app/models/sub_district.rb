982cd6957727ce622dab19115bbdfe493f3c9695
Toby Anderson <toby.anderson@gmail.com>
2016-02-03 13:25:18 +1100

    Fix syntax error in sub-district model.

--- app/models/sub_district.rb
+++ app/models/sub_district.rb
@@ -1,7 +1,7 @@
 class SubDistrict < ActiveRecord::Base
   belongs_to :district
 
-  delegates :geo_state, to: :district
+  delegate :geo_state, to: :district
 
   validates :name, presence: true, allow_nil: false, uniqueness: { scope: :district }
   validates :district, presence: true, allow_nil: false
