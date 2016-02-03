c15ed7b225cee5a6b6694d02460ac95ad8d2269b
Toby Anderson <toby.anderson@gmail.com>
2016-02-03 16:41:34 +1100

    Instead of using 'district_dep' and 'sub_district_dep' use 'district_name' and 'sub_district_name' for events columns.

--- db/migrate/20160202051502_add_sub_district_to_events.rb
+++ db/migrate/20160202051502_add_sub_district_to_events.rb
@@ -2,8 +2,8 @@ class AddSubDistrictToEvents < ActiveRecord::Migration
 
   def change
     add_reference :events, :sub_district, index: true
-    rename_column :events, :district, :district_dep
-    rename_column :events, :sub_district, :sub_district_dep
+    rename_column :events, :district, :district_name
+    rename_column :events, :sub_district, :sub_district_name
   end
 
 end
