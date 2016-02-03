7dac7adfa6f4713f806a52896fb0ee8294c517bd
Toby Anderson <toby.anderson@gmail.com>
2016-02-03 16:38:58 +1100

    Fix errors in LocationBased concern.

--- app/models/concerns/location_based.rb
+++ app/models/concerns/location_based.rb
@@ -2,23 +2,25 @@ module LocationBased
 
   extend ActiveSupport::Concern
 
+  include StateBased
+
   included do
     belongs_to :sub_district
     delegate :district, to: :sub_district
-    validates :sub_district, presence: 
+    validates :sub_district, presence:  true
     validate :location_must_be_in_geo_state
   end
 
   private
 
   def geo_state_init
-    self.geo_state ||= sub_district.geo_state
+    self.geo_state ||= sub_district ? sub_district.geo_state : nil
     super
   end
 
   def location_must_be_in_geo_state
-    if sub_district.geo_state != geo_state
-      errors.add(:sub_district, "must be located within the State")
+    if sub_district and sub_district.geo_state != geo_state
+      errors.add(:sub_district, "is not located within the #{geo_state.name}")
     end
   end
 
