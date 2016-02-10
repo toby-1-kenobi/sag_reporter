ba95b430fd5a78e1c6b2048eaf2bf370a9a16bff
Toby Anderson <toby.anderson@gmail.com>
2016-02-10 21:27:53 +1100

    Improve tests for Person.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -2,20 +2,28 @@ require "test_helper"
 
 describe Person do
 
-  let(:person) { Person.new(name: "Fred") }
+  let(:person) { Person.new(name: "Fred", geo_state: geo_states(:nb)) }
 
-  it "is valid with only a name" do
+  it "is valid with only a name and geo_State" do
     value(person).must_be :valid?
   end
 
   it "is not valid without a name" do
   	person.name = ""
-  	_(person).wont_be :valid?
+    person.valid?
+  	_(person.errors[:name]).must_be :any?
+  end
+
+  it "is not valid without a geo_state" do
+    person.geo_state = nil
+    person.valid?
+    _(person.errors[:geo_state]).must_be :any?
   end
 
   it "doesn't have a very long name" do
   	person.name = "a" * 51
-  	_(person).wont_be :valid?
+    person.valid?
+  	_(person.errors[:name]).must_be :any?
   end
 
   it "may have a 10-digit phone number" do
@@ -25,40 +33,43 @@ describe Person do
 
   it "cannot have a phone number with less than 10 digits" do
   	person.phone = "1" *9
-  	_(person).wont_be :valid?
+    person.valid?
+  	_(person.errors[:phone]).must_be :any?
   end
 
   it "cannot have a phone number with more than 10 digits" do
   	person.phone = "1" *11
-  	_(person).wont_be :valid?
+    person.valid?
+  	_(person.errors[:phone]).must_be :any?
   end
 
   it "cannot have a phone number with letters" do
   	person.phone = "098765432a"
-  	_(person).wont_be :valid?
+    person.valid?
+  	_(person.errors[:phone]).must_be :any?
   end
 
   it "removes white space and hyphens from phone numbers" do
   	person.phone = " 123-45\t6 \n\t7 8-90 "
-  	_(person).must_be :valid?
+    person.valid?
   	_(person.phone).must_equal "1234567890"
   end
 
   it "removes indian prefixes from phone numbers" do
   	person.phone = "(+91) 1234 567 890"
-  	_(person).must_be :valid?
+    person.valid?
   	_(person.phone).must_equal "1234567890"
   	person.phone = "01234567890"
-  	_(person).must_be :valid?
+    person.valid?
   	_(person.phone).must_equal "1234567890"
   end
 
   it "does not remove digits that may look like prefixes from phone numbers" do
   	person.phone = "91 1234 5678"
-  	_(person).must_be :valid?
+    person.valid?
   	_(person.phone).must_equal "9112345678"
   	person.phone = "0123456789"
-  	_(person).must_be :valid?
+    person.valid?
   	_(person.phone).must_equal "0123456789"
   end
 
