1593217339b91442a964ab722be0e8835d21dbf6
Toby Anderson <toby.anderson@gmail.com>
2016-02-19 14:13:24 +1100

    Enable LanguageProgress objects to calculate a range of month scores, and make generating outcome table data more efficient by using this.

--- test/models/state_language_test.rb
+++ test/models/state_language_test.rb
@@ -8,6 +8,8 @@ describe StateLanguage do
   end
 
   it "makes a table of outcome scores" do
+    start_date = Date.new(2015,7,1)
+    end_date = Date.new(2015,12,1)
     pm_social1 = progress_markers(:skills_used)
     pm_social2 = progress_markers(:new_initiatives)
     pm_leader = progress_markers(:devotional)
@@ -17,17 +19,13 @@ describe StateLanguage do
     lp_social2 = LanguageProgress.new progress_marker: pm_social2
     lp_leader = LanguageProgress.new progress_marker: pm_leader
 
-    lp_social1.stubs(:month_score).returns(0)
-    lp_social2.stubs(:month_score).returns(0)
-    lp_leader.stubs(:month_score).returns(0)
-    lp_social1.stubs(:month_score).with(2015, 10).returns(1)
-    lp_social2.stubs(:month_score).with(2015, 10).returns(2)
-    lp_leader.stubs(:month_score).with(2015, 10).returns(4)
-    lp_social2.stubs(:month_score).with(2015, 11).returns(8)
+    lp_social1.stubs(:outcome_scores).with(start_date, end_date).returns({"October 2015" => 1})
+    lp_social2.stubs(:outcome_scores).with(start_date, end_date).returns({"October 2015" => 2, "November 2015" => 8})
+    lp_leader.stubs(:outcome_scores).with(start_date, end_date).returns({"October 2015" => 4})
 
     state_language.language_progresses << [lp_social1, lp_social2, lp_leader]
     state_language.language_progresses.stubs(:includes).returns state_language.language_progresses
-    table = state_language.outcome_table_data(year: 2015, month: 12)
+    table = state_language.outcome_table_data(from_date: start_date, to_date: end_date)
 
     value(table[social_oa.name]["October 2015"]).must_equal 3
     value(table[leader_oa.name]["October 2015"]).must_equal 4
