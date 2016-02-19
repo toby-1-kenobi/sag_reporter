a3238fd4975ec7e523a0486bc57774efafb0acb0
Toby Anderson <toby.anderson@gmail.com>
2016-02-19 11:09:28 +1100

    Make StateLAnguage objects generate outcome table data in a more efficient way than Language objects did it.

--- app/models/state_language.rb
+++ app/models/state_language.rb
@@ -8,4 +8,28 @@ class StateLanguage < ActiveRecord::Base
   validates :geo_state, presence: true
   validates :language, presence: true
 
+  def outcome_table_data(options = {})
+    options[:from_date] ||= 6.months.ago
+    options[:to_date] ||= Date.today
+    dates_by_month = (options[:from_date].to_date..options[:to_date].to_date).select{ |d| d.day == 1}
+
+    table = Hash.new
+    table["Totals"] = Hash.new
+
+    all_lps = language_progresses.includes(:progress_marker => :topic)
+    all_lps.each do |lp|
+      oa_name = lp.progress_marker.topic.name
+      table[oa_name] ||= Hash.new
+      dates_by_month.each do |date|
+        score = lp.month_score(date.year, date.month)
+        table[oa_name][date.strftime("%B %Y")] ||= 0
+        table[oa_name][date.strftime("%B %Y")] += score
+        table["Totals"][date.strftime("%B %Y")] ||= 0
+        table["Totals"][date.strftime("%B %Y")] += score
+      end
+    end
+
+    return table
+  end
+
 end
