ddccb3510be57c71bfd79061f599481f76ad98a2
Toby Anderson <toby.anderson@gmail.com>
2016-02-24 16:57:40 +1100

    Catch RecordNotFound errors and redirect to the home page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   # Access sessions helper and roles helper from application controllers.
   include SessionsHelper
@@ -19,6 +20,11 @@ class ApplicationController < ActionController::Base
         end
       end
       params.except!(:colour_darkness)
+    end
+
+    def record_not_found
+      flash["error"] = "We tried to fetch a record that doesn't exist."
+      redirect_to root_path
     end
 
 end
