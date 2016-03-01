8fc978bf48954b592ff251c01c5259fc341b3287
Toby Anderson <toby.anderson@gmail.com>
2016-03-01 13:24:24 +1100

    Add delegation from planning_report to report. Also removes some unnecessary code from report.

--- app/models/planning_report.rb
+++ app/models/planning_report.rb
@@ -1,8 +1,15 @@
 class PlanningReport < ActiveRecord::Base
+
   enum status: [ :archived, :active ]
+  
   has_one :report, inverse_of: :planning_report
+  delegate :content, to: :report
+  delegate :reporter, to: :report
+  delegate :event, to: :report
+  delegate :report_date, to: :report
 
   def report_type
     "planning"
   end
+
 end
