8119ee51860895d5f40b695a1eaa9d498d7d9b9e
Toby Anderson <toby.anderson@gmail.com>
2016-03-01 14:23:36 +1100

    Add validations to report relationships.

--- app/models/planning_report.rb
+++ app/models/planning_report.rb
@@ -1,12 +1,14 @@
 class PlanningReport < ActiveRecord::Base
 
   enum status: [ :archived, :active ]
-  
+
   has_one :report, inverse_of: :planning_report
   delegate :content, to: :report
   delegate :reporter, to: :report
   delegate :event, to: :report
   delegate :report_date, to: :report
+
+  validates :report, presence: true
 
   def report_type
     "planning"
