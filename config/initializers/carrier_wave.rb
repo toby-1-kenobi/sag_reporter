d11f3c78334560b4d589bc077f7097647a9da2d0
Toby Anderson <toby.anderson@gmail.com>
2016-03-15 22:54:05 +1100

    Bump version number to 0.19.0

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
+      # Configuration for Google Cloud Storage
       :provider              => 'Google',
       :google_storage_access_key_id     => ENV['GOOGLE_ACCESS_KEY'],
       :google_storage_secret_access_key => ENV['GOOGLE_SECRET_KEY']
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 class SagReporter::Application
-  VERSION = '0.18.1'
+  VERSION = '0.19.0'
 end
