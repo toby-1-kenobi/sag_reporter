a7afcce81a423c6f5f8539c4079c8050308ee6a1
Toby Anderson <toby.anderson@gmail.com>
2016-03-15 16:41:47 +1100

    When editing a report show any existing pictures giving the option to remove them.

--- app/assets/javascripts/reports.coffee
+++ app/assets/javascripts/reports.coffee
@@ -23,9 +23,10 @@ addImageInput = ->
 imageListUpdate = ->
   list = ""
   $('.picture-input input').each ->
-    size_in_megabytes = this.files[0].size/1024/1024;
-    if size_in_megabytes > 5
-      alert 'Maximum file size is 5MB. Please choose a smaller file.'
+    if typeof this.files[0] != 'undefined'
+      size_in_megabytes = this.files[0].size/1024/1024;
+      if size_in_megabytes > 5
+        alert 'Maximum file size is 5MB. Please choose a smaller file.'
     filename = $(this).val().split('\\').pop()
     if filename.length > 0
       list += '<li class="collection-item">' +
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [600, 600]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,9 +33,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
