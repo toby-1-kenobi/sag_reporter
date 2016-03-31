d4cec32b63530143fe55c767cacc7f136dcb36f8
Toby Anderson <toby.anderson@gmail.com>
2016-03-31 13:46:03 +1100

    Use full search on autocomplete for district and sub-district names.

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -2,7 +2,7 @@ class DistrictsController < ApplicationController
 
   before_action :require_login
 
-  autocomplete :sub_district, :name
+  autocomplete :sub_district, :name, full: true
 
   def get_autocomplete_items(parameters)
     super(parameters).where(:district_id => params[:district_id])
