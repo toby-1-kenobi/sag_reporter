103206b3004ace45c19ccccb26f788c5b16e5519
Toby Anderson <toby.anderson@gmail.com>
2016-05-06 15:41:51 +1000

    Validate user password to be alphanumeric.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :phone, presence: true, length: { is: 10 }, format: { with: /\A\d+\Z/ }, uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            format: {
+                with: /\A[\d\w]+\Z/im,
+                message: 'must use only letters and numbers'
+            },
+            allow_nil: true
   validates :mother_tongue_id, presence: true, allow_nil: false
   validates :role_id, presence: true, allow_nil: false
   validates :geo_states, presence: true
