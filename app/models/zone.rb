92b351981d0e233aefe2e463f6554214c135cd61
Toby Anderson <toby.anderson@gmail.com>
2016-06-03 16:13:10 +1000

    Add aggregation by zone table to transformation page.

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -3,6 +3,7 @@ class Zone < ActiveRecord::Base
   has_many :geo_states
   has_many :users, through: :geo_states
   has_many :languages, through: :geo_states
+  has_many :state_languages, through: :geo_states
 
   def self.of_states(geo_states)
     geo_states.collect{ |gs| gs.zone }.uniq
