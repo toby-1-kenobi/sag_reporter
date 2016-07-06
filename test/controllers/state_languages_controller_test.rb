c4114f569a1eedc6691b76c2825cdcadb9423356
Toby Anderson <toby.anderson@gmail.com>
2016-07-06 10:52:08 +1000

    Implement spreadsheet export of transformation data

--- test/controllers/state_languages_controller_test.rb
+++ test/controllers/state_languages_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe StateLanguagesController do
-  it "must be a real test" do
-    flunk "Need real tests"
+
+  def setup
+    @user = users(:andrew)
+  end
+
+  it 'must set the outcomes_score variable for transformation spreadsheet' do
+    log_in_as(@user)
+    get :transformation_spreadsheet,
+        year_a: 2016,
+        month_a: 1,
+        year_b: 2016,
+        month_b: 6,
+        format: :csv
+    value(@outcomes_score).wont_be_nil
   end
 end
