16220dd26b7468acfc55e0aea6b4677d6db10c95
Toby Anderson <toby.anderson@gmail.com>
2016-08-31 15:23:54 +1000

    Strengthen the relationships between languages and cluster, data sources and language families.
    Add some states to the fixtures and fix up language families fixtures, updating seeds.db
    
    Add the language data file from Shinu. Add a script to add some of the data t languages (iso code, family, population with source, location text, number of translations, cluster and other info) and import the languages that aren't already in the database.

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -1,4 +1,4 @@
 class DataSource < ActiveRecord::Base
-  has_many :languages
+  has_many :languages, inverse_of: :pop_source, foreign_key: 'pop_source_id', dependent: :nullify
   validates :name, presence: true, allow_nil: false, uniqueness: true
 end
