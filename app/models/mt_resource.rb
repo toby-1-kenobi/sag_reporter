cc6bc07c21e7b1b806ac969347b11322771faa5f
Toby Anderson <toby.anderson@gmail.com>
2016-09-06 14:55:07 +1000

    Add more fields to MT resources model na dadd more options to the category field.

--- app/models/mt_resource.rb
+++ app/models/mt_resource.rb
@@ -2,11 +2,28 @@ class MtResource < ActiveRecord::Base
 
   include StateBased
 
-  enum category: [ :song, :drama, :big_book, :flashcard, :tract, :story, :primer, :dictionary ]
+  enum category: [
+      :song,
+      :drama,
+      :big_book,
+      :flashcard,
+      :tract,
+      :story,
+      :primer,
+      :dictionary,
+      :jesus_film,
+      :new_testament,
+      :whole_bible,
+      :bible_portion,
+      :radio_program,
+      :sunday_school_material,
+      :other ]
+
+  enum status: [ :unknown, :work_in_progress, :published ]
 
   belongs_to :user
   belongs_to :language
   has_many :contributions, class_name: 'Creation', dependent: :destroy
-  has_many :contributers, through: :contributions, source: "person"
+  has_many :contributers, through: :contributions, source: 'person'
 
 end
