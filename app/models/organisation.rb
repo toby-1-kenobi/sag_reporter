2723cf74985b6a07bc7bebe667797c4237797517
Toby Anderson <toby.anderson@gmail.com>
2016-09-15 11:49:37 +1000

    Extend language data import to add link organisations to languages.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,9 +2,9 @@ class Organisation < ActiveRecord::Base
   belongs_to :parent, class_name: 'Organisation', inverse_of: :children
   has_many :children, class_name: 'Organisation', inverse_of: :parent
   has_many :language_engagements, class_name: 'OrganisationEngagement'
-  has_many :engaged_languages, through: :language_engagements
+  has_many :engaged_languages, through: :language_engagements, source: :language
   has_many :language_translations, class_name: 'OrganisationTranslation'
-  has_many :translating_languages, through: :language_translations
+  has_many :translating_languages, through: :language_translations, source: :language
   validates :name, presence: true, allow_blank: false, allow_nil: false, uniqueness: true
   validates :abbreviation, uniqueness: true, allow_nil: true
 end
