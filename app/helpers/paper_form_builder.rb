2acbd3c4fc05d3c6618e96d91e9cdc79a60c473f
Toby Anderson <toby.anderson@gmail.com>
2016-10-05 12:40:16 +0530

    Improve the paper form builder and add family and cluster to the language edit view and change the layout slightly.

--- app/helpers/paper_form_builder.rb
+++ app/helpers/paper_form_builder.rb
@@ -7,11 +7,18 @@ class PaperFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def paper_autocomplete(method, options = {})
-    PaperAutocomplete.new(@object_name, method, @template, options).render
+    autocomplete = PaperAutocomplete.new(@object_name, method, @template, options).render
+  end
+  def paper_autocomplete_js(method, options = {})
+    autocomplete = PaperAutocomplete.new(@object_name, method, @template, options).renderjs
   end
 
   def paper_textarea(method, options = {})
     PaperTextArea.new(@object_name, method, @template, options).render
+  end
+
+  def paper_checkbox(method, options = {})
+    PaperCheckbox.new(@object_name, method, @template, options).render
   end
 
   class PaperTextField < ActionView::Helpers::Tags::Base
@@ -27,6 +34,39 @@ class PaperFormBuilder < ActionView::Helpers::FormBuilder
       options['label'] ||= @method_name.humanize
       add_default_name_and_id(options)
       tag('paper-input', options)
+    end
+
+  end
+
+  class PaperCheckbox < ActionView::Helpers::Tags::CheckBox
+
+    def initialize(object_name, method_name, template_object, options = {})
+      super(object_name, method_name, template_object, 1, 0, options)
+    end
+
+    def render
+      options = @options.stringify_keys
+      options['value']    = @checked_value
+      options['checked'] = 'checked' if input_checked?(object, options)
+      label = options.fetch('label', @method_name.humanize)
+      options.delete('label')
+
+      if options['multiple']
+        add_default_name_and_id_for_value(@checked_value, options)
+        options.delete('multiple')
+      else
+        add_default_name_and_id(options)
+      end
+
+      include_hidden = options.delete('include_hidden') { true }
+      checkbox = content_tag('paper-checkbox', label, options)
+
+      if include_hidden
+        hidden = hidden_field_for_checkbox(options)
+        hidden + checkbox
+      else
+        checkbox
+      end
     end
 
   end
@@ -60,23 +100,23 @@ class PaperFormBuilder < ActionView::Helpers::FormBuilder
       options['value'] = options.fetch('value') { value_before_type_cast(object) }.name
       options['label'] ||= @method_name.humanize
       add_default_name_and_id(options)
-      render_html(options) + "\n" + @template_object.javascript_tag(render_js(options))
-    end
-
-    def render_html(options)
       tag('paper-autocomplete', options)
     end
 
-    def render_js(options)
+    def renderjs()
+      options = @options.stringify_keys
+      options['value'] = options.fetch('value') { value_before_type_cast(object) }.name
+      add_default_name_and_id(options)
       list = value(retrieve_object(false)).class.all.to_a.map{ |item| {text: item.name, value: item.id} }
       selected = list.select{ |item| item[:text] == options['value'] }.first
-      "HTMLImports.whenReady(function(){" +
+      js = "window.addEventListener('WebComponentsReady', function(){" +
           "\n  var items = #{list.to_json};" +
           "\n  var autocomplete_field = document.querySelector('##{options['id']}');" +
           "\n  autocomplete_field.source = items;" +
           "\n  autocomplete_field.setOption(#{selected.to_json});" +
           "\n  autocomplete_field.enable();" +
           "\n});"
+      @template_object.javascript_tag(js)
     end
 
   end
