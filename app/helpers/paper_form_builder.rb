63009bbe1578d43f92c0bc4b42d8a3e4afd42082
Toby Anderson <toby.anderson@gmail.com>
2016-10-06 12:50:07 +0530

    Add radio buttons to the language edit form.

--- app/helpers/paper_form_builder.rb
+++ app/helpers/paper_form_builder.rb
@@ -21,6 +21,12 @@ class PaperFormBuilder < ActionView::Helpers::FormBuilder
     PaperCheckbox.new(@object_name, method, @template, options).render
   end
 
+  def paper_radio_group(method, options = {})
+    PaperRadioGroup.new(@object_name, method, @template, options).render
+  end
+
+
+
   class PaperTextField < ActionView::Helpers::Tags::Base
 
     def initialize(object_name, method_name, template_object, options = {})
@@ -84,6 +90,39 @@ class PaperFormBuilder < ActionView::Helpers::FormBuilder
       options['label'] ||= @method_name.humanize
       add_default_name_and_id(options)
       tag('paper-textarea', options)
+    end
+
+  end
+
+  class PaperRadioGroup < ActionView::Helpers::Tags::Base
+
+    def initialize(object_name, method_name, template_object, options = {})
+      super(object_name, method_name, template_object, options)
+    end
+
+    def render
+      options = @options.stringify_keys
+      options['selected'] = options.fetch('selected') { value_before_type_cast(object) }
+      options['label'] ||= @method_name.humanize
+      label = options.delete('label')
+      # vertical alignment of radio buttons using iron-flex-layout
+      options['class'] ||= ''
+      options['class'] += ' layout vertical start'
+      options['class'].strip!
+      # set the selected attribute to be the value, not the name
+      options['attr-for-selected'] = 'value'
+      add_default_name_and_id(options)
+      name = options.delete('name')
+      # assumption is that all valid values are stored as an enum on the model
+      # that can be accessed with the plural of the method name
+      collection = retrieve_object(false).class.send(@method_name.pluralize).keys
+      radios = "\n<label>#{label}</label>"
+      collection.each do |item|
+        radios += content_tag('paper-radio-button', item.humanize, name: name, value: item) + "\n"
+      end
+      # paper-radio-group has non-boolean 'selected' attribute
+      BOOLEAN_ATTRIBUTES.delete('selected')
+      content_tag('paper-radio-group', radios.html_safe, options)
     end
 
   end
