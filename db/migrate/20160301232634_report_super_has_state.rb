fd91ac73a498ec99ea94634ab9d642b3d380973e
Toby Anderson <toby.anderson@gmail.com>
2016-11-09 17:11:38 +0530

    Clean up migrations 20160301045323 to 20160302003637

--- db/migrate/20160301232634_report_super_has_state.rb
+++ db/migrate/20160301232634_report_super_has_state.rb
@@ -1,37 +1,13 @@
 class ReportSuperHasState < ActiveRecord::Migration
 
   def up
-    puts "This migration will do nothing if ImpactReports delegates :state to Report."
-    puts "are you sure you want to continue? [Y/n]"
-    answer = $stdin.gets.chomp
-    if answer != 'y' and answer != 'Y'
-      fail "Make sure ImpactReports does not delegate :state to Report, then try again."
-    end
     remove_column :planning_reports, :status
     remove_column :challenge_reports, :status
-    ImpactReport.find_each do |ir|
-      report = ir.report
-      report.state = ir.state
-      report.save!
-    end
     remove_column :impact_reports, :state
   end
 
   def down
-    puts "This migration will do nothing if ImpactReports delegates :state to Report."
-    puts "are you sure you want to continue? [Y/n]"
-    answer = $stdin.gets.chomp
-    if answer != 'y' and answer != 'Y'
-      fail "Make sure ImpactReports does not delegate :state to Report, then try again."
-    end
     add_column :impact_reports, :state, :integer
-    ImpactReport.find_each do |ir|
-      state = ir.report.state
-      state = 1 if state == "active"
-      state = 0 if state == "archived"
-      ir.state = state
-      ir.save!
-    end
     add_column :challenge_reports, :status, :integer
     add_column :planning_reports, :status, :integer
   end
--- db/migrate/20160302003637_add_status_to_report.rb
+++ db/migrate/20160302003637_add_status_to_report.rb
@@ -2,29 +2,11 @@ class AddStatusToReport < ActiveRecord::Migration
 
   def up
     add_column :reports, :status, :integer, null: false, default: 0
-    Report.find_each do |report|
-      state = report.state
-      if state == "archived" or state == 0
-        report.status = 1
-      else
-        report.status = 0
-      end
-      report.save!
-    end
     remove_column :reports, :state
   end
 
   def down
     add_column :reports, :state, :integer, null: false, default: 1
-    Report.find_each do |report|
-      status = report.status
-      if status == "archived" or status == 1
-        report.state = 0
-      else
-        report.state = 1
-      end
-      report.save!
-    end
     remove_column :reports, :status
   end
 
