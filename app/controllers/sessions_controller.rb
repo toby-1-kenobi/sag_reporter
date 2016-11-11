5ba70c0846ac91acb8c495fd8d179fd64f7c1cec
bcs_dev <uday.kumar@bridgeconn.com>
2016-11-11 14:57:02 +0530

    send otp method added with twilio

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :check_user, only: [:two_factor_auth, :new]
+
   def new
   end
 
@@ -9,11 +11,7 @@ class SessionsController < ApplicationController
       @password = params[:session][:password]
       @otp_code = user.otp_code
       session[:temp_user] = user.id
-      # TWILIO.messages.create(
-      #   from: '+15856450851',
-      #   to: "+91"+@phone.to_s,
-      #   body: 'Your LCI verification code is:'+@otp_code
-      # )
+      send_otp(user)
       flash.now['info'] = "OTP has been sent in your registered mobile number"
     else
       flash.now['error'] = 'Phone number or password not correct'
@@ -23,18 +21,18 @@ class SessionsController < ApplicationController
 
   def verify_otp
     user = User.find_by(id: session[:temp_user])
-    if user && user.authenticate_otp(params[:otp_code])
-      render json: { success: true }
+    if user && user.authenticate_otp(params[:otp_code], drift:60)
+      render json: { success: true, message: "OTP sent successfully." }
     else
-      render json: { success: false, message: "OTP has expired please click on resend otp and try to login again" }
+      render json: { success: false, message: "OTP has expired or you have entered wrong OTP. please click on resend OTP and try to login again." }
     end
   end
 
   def resend_otp
     user = User.find_by(id: session[:temp_user]) if session[:temp_user]
-    code = user.otp_code
-    if code
-      render json: { success: true, message: "OTP sent successfully.", otp_code: code }
+    if user
+      send_otp(user)
+      render json: { success: true, message: "OTP sent successfully." }
     else
       render json: { success: false, message: "Oops. Something went wrong. Please try later." }
     end
@@ -43,6 +41,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(phone: params[:session][:phone])
     if user && user.authenticate(params[:session][:password])
+        session[:temp_user] = nil
         log_in user
         remember user
         if params[:session][:password] == 'password'
@@ -62,5 +61,19 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to login_url
+  end
+
+  private
+
+  def send_otp(user)
+    TWILIO.messages.create(
+      from: ENV['PHONE_NUMBER'],
+      to: "+91"+user.phone.to_s,
+      body: 'Your LCI verification code is:'+user.otp_code.to_s
+    )
+  end
+
+  def check_user
+     redirect_to root_path if logged_in?
   end
 end
