4f6a6c9b7147212cba1ece50a2e48a5884eed7fd
bcs_dev <uday.kumar@bridgeconn.com>
2016-11-11 18:13:26 +0530

    add sendgrid code in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ActionMailer::Base
   require 'sendgrid-ruby'
   include SendGrid
-
-  default :from => "me@mydomain.com"
+  default :from => "info@lci.com"
 
   def user_email_confirmation(user)
+  	headers['X-SMTPAPI'] = {
+      category: ['emailconfirm']
+    }.to_json
     @user = user
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Email Confirmation")
   end
