724ca9f77bf3d3233ccb8f5fc9f76a4767588052
bcs_dev <uday.kumar@bridgeconn.com>
2016-11-14 14:51:40 +0530

    add method to send otp in mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ActionMailer::Base
   require 'sendgrid-ruby'
   include SendGrid
   default :from => "info@lci.com"
-
   def user_email_confirmation(user)
   	headers['X-SMTPAPI'] = {
       category: ['emailconfirm']
@@ -10,4 +9,15 @@ class UserMailer < ActionMailer::Base
     @user = user
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Email Confirmation")
   end
+
+  def user_otp_code(user, otp_code)
+    headers['X-SMTPAPI'] = {
+      category: ['emailconfirm']
+    }.to_json
+    @otp_code = otp_code
+    @user = user
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "OTP login code")
+  end
+
+
 end
