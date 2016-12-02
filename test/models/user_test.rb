9b5ba8303ee40f07adaf985c2701c90e115badf6
Toby Anderson <toby.anderson@gmail.com>
2016-12-02 15:35:27 +0530

    Add restrictions against user objects being destroyed if other objects depend upon it.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,84 +4,143 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(
-      name: "Example User", 
-      phone: "9876543210", 
-      password: "foobar", 
-      password_confirmation: "foobar",
+      name: 'Example User',
+      phone: '9876543210',
+      password: 'foobar',
+      password_confirmation: 'foobar',
       role: Role.take,
       mother_tongue: Language.take)
     @user.geo_states << geo_states(:nb)
   end
+
+  it 'must be valid' do
+    value(@user).must_be :valid?
+  end
   
-  it "can have multiple geo_states" do
+  it 'can have multiple geo_states' do
     @user.geo_states << geo_states(:assam)
-    _(@user.geo_states.length).must_be :>, 1
+    value(@user.geo_states.length).must_be :>, 1
   end
 
-  test "should be valid" do
-    assert @user.valid?
+  it 'wont be destroyed with report' do
+    report = Report.new(
+        reporter: @user,
+        content: 'hi',
+        impact_report: impact_reports(:'impact-report-1')
+    )
+    report.save!
+    @user.destroy
+    value(@user).must_be :persisted?
   end
 
-  test "blank name is invalid" do
-    @user.name = "     "
-    assert_not @user.valid?
+  it 'wont be destroyed with event' do
+    event = Event.new(
+        event_label: 'hi',
+        event_date: Time.now,
+        participant_amount: 0,
+        record_creator: @user
+    )
+    event.save!
+    @user.destroy
+    value(@user).must_be :persisted?
   end
 
-  test "blank phone is invalid" do
-    @user.phone = "     "
-    assert_not @user.valid?
+  it 'wont be destroyed with person' do
+    person = Person.new(name: 'joe', record_creator: @user)
+    person.save!
+    @user.destroy
+    value(@user).must_be :persisted?
   end
 
-  test "name should not be too long" do
-    @user.name = "a" * 51
-    assert_not @user.valid?
+  it 'wont be destroyed with progress update' do
+    pu = ProgressUpdate.new(
+        progress: 1,
+        month: 1,
+        year: 2016,
+        user: @user,
+        language_progress: language_progresses(:aka_skills_used)
+    )
+    pu.save!
+    @user.destroy
+    value(@user).must_be :persisted?
+  end
+
+  it 'wont be destroyed with output count' do
+    oc = output_counts(:output_count_1)
+    oc.user = @user
+    oc.save!
+    @user.destroy
+    value(@user).must_be :persisted?
+  end
+
+  it 'wont be destroyed with mt resource' do
+    resource = mt_resources(:'resource-1')
+    resource.user = @user
+    resource.save!
+    @user.destroy
+    value(@user).must_be :persisted?
+  end
+
+  it 'wont be valid with a blank name' do
+    @user.name = '     '
+    value(@user).wont_be :valid?
+  end
+
+  it 'wont be valid with a blank phone number' do
+    @user.phone = '     '
+    value(@user).wont_be :valid?
+  end
+
+  it 'wont be valid with a long name' do
+    @user.name = 'a' * 51
+    value(@user).wont_be :valid?
   end
 
   # In India mobile phone numbers are 10 digits
-  test "phone number should be length 10" do
-    @user.phone = "1" * 11
-    assert_not @user.valid?
-    @user.phone = "1" * 9
-    assert_not @user.valid?
+  it "wont be valid with a phone number that's not 10 digits long" do
+    @user.phone = '1' * 11
+    value(@user).wont_be :valid?
+    @user.phone = '1' * 9
+    value(@user).wont_be :valid?
   end
 
-  test "phone number should have 10 digits" do
-    @user.phone = "a" * 10
-    assert_not @user.valid?
-    @user.phone = "+123456789"
-    assert_not @user.valid?
-    @user.phone = "1234 56789"
-    assert_not @user.valid?
+  it "wont be valid with a phone number that's not all digits" do
+    @user.phone = 'a' * 10
+    value(@user).wont_be :valid?
+    @user.phone = '+123456789'
+    value(@user).wont_be :valid?
+    @user.phone = '1234 56789'
+    value(@user).wont_be :valid?
   end
 
-  test "phone number may have prefixes digits" do
-    @user.phone = "+91 0123-456-789"
-    assert @user.valid?
-    @user.phone = "01234 567890"
-    assert @user.valid?
-    @user.phone = "91 0123-4-789"
-    assert @user.valid?
-    @user.phone = "0012 347 895"
-    assert @user.valid?
+  it "must be valid with a phone number that's got a valid prefix" do
+    @user.phone = '+91 0123-456-789'
+    value(@user).must_be :valid?
+    @user.phone = '01234 567890'
+    value(@user).must_be :valid?
+    @user.phone = '91 0123-4-789'
+    value(@user).must_be :valid?
+    @user.phone = '0012 347 895'
+    value(@user).must_be :valid?
   end
 
-  test "phone numbers should be unique" do
+  it 'wont be valid with a duplicate phone number' do
     duplicate_user = @user.dup
     @user.save
-    assert_not duplicate_user.valid?
+    value(duplicate_user).wont_be :valid?
   end
 
-  test "password should not be blank" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
+  it 'wont be valid with a blank password' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    value(@user).wont_be :valid?
   end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
+  it 'wont be valid with a too short password' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    value(@user).wont_be :valid?
   end
   
-  test "authenticated? should return false for a user with nil digest" do
+  test 'authenticated? should return false for a user with nil digest' do
     assert_not @user.authenticated?('')
   end
 
