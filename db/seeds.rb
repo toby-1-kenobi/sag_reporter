d881e56b1837fd84bee6688a38ce9930abe1becc
Toby Anderson <toby.anderson@gmail.com>
2016-12-29 15:05:46 +0530

    Change the import script for baseline data so it doesn't try to mtch on progress marker descriptions, but just uses the number instead.

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,21 +29,21 @@ include FixtureParser
 fixtures_dir = "#{Rails.root}/db/seed_fixtures"
 test_fixtures_dir = "#{Rails.root}/test/fixtures"
 options_hash = {
-"#{fixtures_dir}/permissions.yml" => { model_name: 'Permission', key_fields: ['name'], update?: false },
-"#{fixtures_dir}/roles.yml" => { model_name: 'Role', key_fields: ['name'], update?: false },
-  "#{fixtures_dir}/zones.yml" => { model_name: 'Zone', key_fields: ['name'], update?: false },
-  "#{fixtures_dir}/geo_states.yml" => { model_name: 'GeoState', key_fields: ['name'], update?: false },
- "#{fixtures_dir}/purposes.yml" =>  { model_name: 'Purpose', key_fields: ['name'], update?: false },
-  "#{fixtures_dir}/language_families.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
-  "#{fixtures_dir}/languages.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
-  "#{fixtures_dir}/state_languages.yml" => { model_name: 'StateLanguage', key_fields: ['language', 'geo_state'], update?: false },
- "#{fixtures_dir}/translatables.yml" => { model_name: 'Translatable', key_fields: ['identifier'], update?: false },
-  "#{fixtures_dir}/organisations.yml" => { model_name: 'Organisation', key_fields: ['name'], update?: false },
- "#{fixtures_dir}/translations.yml" => { model_name: 'Translation', key_fields: ['id'], update?: false },
+# "#{fixtures_dir}/permissions.yml" => { model_name: 'Permission', key_fields: ['name'], update?: false },
+# "#{fixtures_dir}/roles.yml" => { model_name: 'Role', key_fields: ['name'], update?: false },
+#   "#{fixtures_dir}/zones.yml" => { model_name: 'Zone', key_fields: ['name'], update?: false },
+#   "#{fixtures_dir}/geo_states.yml" => { model_name: 'GeoState', key_fields: ['name'], update?: false },
+#  "#{fixtures_dir}/purposes.yml" =>  { model_name: 'Purpose', key_fields: ['name'], update?: false },
+#   "#{fixtures_dir}/language_families.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
+#   "#{fixtures_dir}/languages.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
+#   "#{fixtures_dir}/state_languages.yml" => { model_name: 'StateLanguage', key_fields: ['language', 'geo_state'], update?: false },
+#  "#{fixtures_dir}/translatables.yml" => { model_name: 'Translatable', key_fields: ['identifier'], update?: false },
+#   "#{fixtures_dir}/organisations.yml" => { model_name: 'Organisation', key_fields: ['name'], update?: false },
+#  "#{fixtures_dir}/translations.yml" => { model_name: 'Translation', key_fields: ['id'], update?: false },
 	"#{fixtures_dir}/topics.yml" => { model_name: 'Topic', key_fields: ['name'], update?: false },
- "#{fixtures_dir}/progress_markers.yml" => { model_name: 'ProgressMarker', key_fields: ['name'], update?: false },
- "#{fixtures_dir}/output_tallies.yml" => { model_name: 'OutputTally', key_fields: ['name'], update?: false },
-	"#{test_fixtures_dir}/users.yml" => { model_name: 'User', key_fields: ['phone'], update?: false }
+ "#{fixtures_dir}/progress_markers.yml" => { model_name: 'ProgressMarker', key_fields: ['name'], update?: true }
+ # "#{fixtures_dir}/output_tallies.yml" => { model_name: 'OutputTally', key_fields: ['name'], update?: false },
+	# "#{test_fixtures_dir}/users.yml" => { model_name: 'User', key_fields: ['phone'], update?: false }
 }
 parse_fixtures options_hash
 
