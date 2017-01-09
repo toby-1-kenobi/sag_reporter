bd9b12c8a1f3cf4116fed22de24406de85e087d8
Silas <redcl14@gmail.com>
2017-01-09 16:24:21 +0100

    delete hello-world-message

--- test/features/multiple_states_test.rb
+++ test/features/multiple_states_test.rb
@@ -5,7 +5,7 @@ feature "MultipleStates" do
     visit root_path
     page.wont_have_selector("#user-geo_states-dropdown .geo_states-option", :visible)
     page.wont_have_content "Goobye All!"
-    puts "Hello World!"
+#    puts "Hello World!"
 
     page.all('body script', visible: false).each do |el|
       puts 'script: '
