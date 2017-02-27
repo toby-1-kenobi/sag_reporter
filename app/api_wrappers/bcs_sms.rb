b82de5da8229db46599d5b5c3e2c8b957f09d89e
Toby Anderson <toby.anderson@gmail.com>
2017-02-27 23:41:00 +0530

    Implemented a polling system to check for when login code has been sent to the phone. This is necessary beacuse the BCS server seems to take a long time to respond to requests, and can cause timeouts.

--- app/api_wrappers/bcs_sms.rb
+++ app/api_wrappers/bcs_sms.rb
@@ -7,13 +7,41 @@ class BcsSms
   base_uri 'smsserver.bridgeconn.com'
   format :json
 
+  # keep track of background process waiting for a response
+  # use thread id as keys
+  @requests_waiting = Hash.new
+
+  # start a thread that sends the OTP and return the id of that thread
   def self.send_otp(phone_number, otp_code)
-    post '/send_otp', body: {
-        mobile_number: BcsSms.encrypt(phone_number),
-        otp: BcsSms.encrypt(otp_code),
-        secret_key: BcsSms.encrypt(Rails.application.secrets.sms_api_key),
-        secret_token: BcsSms.encrypt(Rails.application.secrets.sms_api_token)
-    }
+    thr = Thread.new do
+      @requests_waiting[Thread.current.object_id] = :pending
+      begin
+        response = post '/send_otp', body: {
+            mobile_number: BcsSms.encrypt(phone_number),
+            otp: BcsSms.encrypt(otp_code),
+            secret_key: BcsSms.encrypt(Rails.application.secrets.sms_api_key),
+            secret_token: BcsSms.encrypt(Rails.application.secrets.sms_api_token)
+        }
+        @requests_waiting[Thread.current.object_id] = response.parsed_response
+      rescue => e
+        @requests_waiting[Thread.current.object_id] = {'status' => 'error', 'message' => e.message}
+      ensure
+        ActiveRecord::Base.connection.close
+      end
+    end
+    return thr.object_id
+  end
+
+  def self.poll(thread_id)
+    if @requests_waiting[thread_id].present?
+      if @requests_waiting[thread_id] == :pending
+        return { 'status' => 'pending' }
+      else
+        return @requests_waiting.delete(thread_id)
+      end
+    else
+      return { 'status' => 'invalid ticket' }
+    end
   end
 
   def self.success? (response)
