540c629780f36950c4104363c2e270ea70d48d69
Toby Anderson <toby.anderson@gmail.com>
2017-03-11 21:48:19 +0530

    Add language maps to the language show page. Also add an image for the impact story title.

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -1,2 +1,32 @@
 module LanguagesHelper
+
+
+
+  # get the string to link the map image for a language
+  # return false if the image can't be found
+  def get_map(language)
+    # the file name is based on the language iso code
+    if Rails.env.production? or Rails.env.development?
+      # for production maps are stored in the cloud
+      map_uri = "https://storage.googleapis.com/lci-language-maps/#{language.iso}.png"
+      require 'open-uri'
+      begin
+        open map_uri
+      rescue => e
+        Rails.logger.debug("no map: #{e.message}")
+        return false
+      end
+      map_uri
+    else
+      # for test and development maps are stored locally
+      link_path = '/uploads/maps'
+      storage_path = "public#{link_path}"
+      if language.iso.present? and File.exists?("#{storage_path}/#{language.iso}.png")
+        "#{link_path}/#{language.iso}.png"
+      else
+        false
+      end
+    end
+  end
+
 end
