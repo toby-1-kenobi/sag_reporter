d66bc9b8c485744b0bf897df3fc5687ceb786583
Toby Anderson <toby.anderson@gmail.com>
2017-05-10 17:18:32 +0530

    Change how the finish line progress statuses get shown.

--- app/models/finish_line_progress.rb
+++ app/models/finish_line_progress.rb
@@ -3,7 +3,7 @@ class FinishLineProgress < ActiveRecord::Base
   enum status: {
       # 0-3 are options for not done markers
       no_need: 0,
-      potential_need: 1,
+      possible_need: 1,
       expressed_needs: 2,
       in_progress: 3,
       # 4-6 are options for done markers
@@ -26,7 +26,16 @@ class FinishLineProgress < ActiveRecord::Base
   end
 
   def human_status
-    "#{complete? ? 'Completed' : 'Not completed'}, #{status.humanize}"
+    case status
+      when 'no_need'
+        'No need'
+      when 'possible_need', 'expressed_needs'
+        "#{status.humanize}, not started"
+      when 'in_progress'
+        'In progress, not completed'
+      else
+        "Completed, #{status.humanize}"
+    end
   end
 
   def self.human_of_status(status)
