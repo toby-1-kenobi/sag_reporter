dc0fdc6a38b0fc71dbbd4be9320ec4b0890bd82a
Toby Anderson <toby.anderson@gmail.com>
2017-07-17 15:03:39 +0530

    Change the version number to "develop" in the developer environment, and remove the database config file from the repository.

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 class SagReporter::Application
   #TODO: change the short name to "LCI App" and update the value in all the reports in the live db
   APP_SHORT_NAME = 'LCIR'
-  VERSION = '2.1.1'
+  VERSION = 'develop'
 end
