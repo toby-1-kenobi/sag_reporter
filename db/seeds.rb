07d88b7ae1a054087065e7af93931541d337aa72
Toby Anderson <toby.anderson@gmail.com>
2017-07-21 16:40:30 +0530

    Use environment variables for db credentials in example_database.yml. Also fix up  the seed.rb file so it includes all seed data and calls other import scripts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#require 'active_record/fixtures'
-
-#ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "languages")
-#ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "permissions")
-#ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "roles")
-#ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "topics")
-#ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/db/seed_fixtures", "users")
-
-#admin_role = Role.create(name: "admin")
-#admin_role.permissions = Permission.all
-
-#User.create!(name:  "admin",
-#             phone: "1234567890",
-#             password:              "password",
-#             password_confirmation: "password",
-#             role: admin_role)
-
 require_relative 'fixture_parser'
 include FixtureParser
 
 fixtures_dir = "#{Rails.root}/db/seed_fixtures"
-test_fixtures_dir = "#{Rails.root}/test/fixtures"
+
 options_hash = {
- #  "#{fixtures_dir}/zones.yml" => { model_name: 'Zone', key_fields: ['name'], update?: false },
- #  "#{fixtures_dir}/geo_states.yml" => { model_name: 'GeoState', key_fields: ['name'], update?: false },
- # "#{fixtures_dir}/purposes.yml" =>  { model_name: 'Purpose', key_fields: ['name'], update?: false },
- #  "#{fixtures_dir}/language_families.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
- #  "#{fixtures_dir}/languages.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
- #  "#{fixtures_dir}/state_languages.yml" => { model_name: 'StateLanguage', key_fields: ['language', 'geo_state'], update?: false },
- #  "#{fixtures_dir}/organisations.yml" => { model_name: 'Organisation', key_fields: ['name'], update?: false },
-	# "#{fixtures_dir}/topics.yml" => { model_name: 'Topic', key_fields: ['name'], update?: false },
- # "#{fixtures_dir}/progress_markers.yml" => { model_name: 'ProgressMarker', key_fields: ['name'], update?: false },
-   "#{fixtures_dir}/finish_line_markers.yml" => { model_name: 'FinishLineMarker', key_fields: ['name'], update?: false }
- # "#{fixtures_dir}/output_tallies.yml" => { model_name: 'OutputTally', key_fields: ['name'], update?: false },
-	# "#{fixtures_dir}/users.yml" => { model_name: 'User', key_fields: ['phone'], update?: false }
+  "#{fixtures_dir}/zones.yml" => { model_name: 'Zone', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/geo_states.yml" => { model_name: 'GeoState', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/language_families.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/languages.yml" => { model_name: 'Language', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/state_languages.yml" => { model_name: 'StateLanguage', key_fields: ['language', 'geo_state'], update?: false },
+  "#{fixtures_dir}/organisations.yml" => { model_name: 'Organisation', key_fields: ['name'], update?: false },
+	"#{fixtures_dir}/topics.yml" => { model_name: 'Topic', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/progress_markers.yml" => { model_name: 'ProgressMarker', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/finish_line_markers.yml" => { model_name: 'FinishLineMarker', key_fields: ['name'], update?: false },
+  "#{fixtures_dir}/translatables.yml" => { model_name: 'Translatable', key_fields: ['identifier'], update?: false },
+  "#{fixtures_dir}/translations.yml" => { model_name: 'Translation', key_fields: ['translatable', 'language'], update?: false }
+# "#{fixtures_dir}/output_tallies.yml" => { model_name: 'OutputTally', key_fields: ['name'], update?: false },
+# "#{fixtures_dir}/purposes.yml" =>  { model_name: 'Purpose', key_fields: ['name'], update?: false },
+# "#{fixtures_dir}/users.yml" => { model_name: 'User', key_fields: ['phone'], update?: false }
 }
 parse_fixtures options_hash
 
@@ -60,3 +45,7 @@ if User.where(role_id: Role.find_by_name('admin')).count == 0
     puts 'Could not create admin user'
   end
 end
+
+require_relative 'location_import'
+require_relative 'language_data_import'
+require_relative 'losa_import'
