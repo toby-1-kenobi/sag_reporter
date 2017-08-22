7483d1c536edac589083c98eba57df92e97ad963
Toby Anderson <toby.anderson@gmail.com>
2017-08-22 16:36:47 +0530

    Create a new mail template for emailing reports to people.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,35 @@
 class UserMailer < ActionMailer::Base
   require 'sendgrid-ruby'
   include SendGrid
-  default :from => "info@lci.com"
+  default :from => 'info@lci.com'
   def user_email_confirmation(user)
   	headers['X-SMTPAPI'] = {
-      category: ['emailconfirm']
+      category: ['lciemailconfirm']
     }.to_json
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Email Confirmation")
+    mail(:to => "#{user.name} <#{user.email}>", :subject => 'Email Confirmation')
   end
 
   def user_otp_code(user, otp_code)
     headers['X-SMTPAPI'] = {
-      category: ['emailconfirm']
+      category: ['lcilogincode']
     }.to_json
     @otp_code = otp_code
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "OTP login code")
+    mail(:to => "#{user.name} <#{user.email}>", :subject => 'OTP login code')
   end
 
+  def user_report(recipient, report)
+    headers['X-SMTPAPI'] = {
+        category: ['lcireport']
+    }.to_json
+    @report = report
+    if recipient.class == User
+      @recipient = recipient
+      mail(:to => "#{recipient.name} <#{recipient.email}>", :subject => 'New LCI report')
+    else
+      mail(:to => recipient, :subject => 'New LCI report')
+    end
+  end
 
 end
