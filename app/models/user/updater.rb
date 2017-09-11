0a06957a5f6062f5d8c18ae2fca8172da6d52222
Toby Anderson <toby.anderson@gmail.com>
2017-09-11 18:30:10 +0530

    Fix bug where removing all curated states, or all spoken languages, or all championed languages from a user doesn't have any effect.

--- app/models/user/updater.rb
+++ app/models/user/updater.rb
@@ -8,9 +8,12 @@ class User::Updater
 
   def update_user(params, skip_confirm_email = false)
     champion = params.delete(:champion)
+    @instance.championed_languages.clear
     speaks = params.delete(:speaks)
+    @instance.spoken_languages.clear
     geo_states = params.delete(:geo_states)
     curated_states = params.delete(:curated_states)
+    @instance.curated_states.clear
     if skip_confirm_email
       # if we skip sending the confirmation email, we must make the email confirmed if it's there
       if params[:email].present?
@@ -22,13 +25,11 @@ class User::Updater
       result = @instance.update_attributes(params)
     end
     if champion
-      @instance.championed_languages.clear
       champion.each do |lang_id|
         @instance.championed_languages << Language.find(lang_id)
       end
     end
     if speaks
-      @instance.spoken_languages.clear
       speaks.each do |lang_id|
         @instance.spoken_languages << Language.find(lang_id)
       end
@@ -43,7 +44,6 @@ class User::Updater
       end
     end
     if curated_states
-      @instance.curated_states.clear
       curated_states.each do |state_id|
         @instance.curated_states << GeoState.find(state_id)
       end
