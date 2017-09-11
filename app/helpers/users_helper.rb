e7563c71c203a83a15eb3e12e2e220c51eb35d2c
Toby Anderson <toby.anderson@gmail.com>
2017-09-11 15:29:18 +0530

    Show the language champion at the top of the language details page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  # get a link to a user by name
+  # don't mention the user name if the logged in user
+  # doesn't have high-sensitivity access
+  # assume there's a logged in user.
+  def user_link(user)
+    anchor_text = (logged_in_user.trusted? or logged_in_user?(user)) ? user.name : "user ##{user.id}"
+    link_to anchor_text, user_path(user)
+  end
+
 end
