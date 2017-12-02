b2d8d0d0781e5b97285597d9e1a8ca368c7714c1
Silas-Germany <silas@wieberneit.de>
2017-12-02 13:28:27 +0530

    Leaving out the status (as it needs the redirect status)

--- app/controllers/external_device_controller.rb
+++ app/controllers/external_device_controller.rb
@@ -202,7 +202,7 @@ class ExternalDeviceController < ApplicationController
         render json: {error: "Uploaded file not found"}, status: :forbidden
         return
       end
-      redirect_to uploaded_file.ref.path, status: :ok
+      redirect_to uploaded_file.ref.path
     rescue => e
       send_message = {error: e.to_s, where: e.backtrace.to_s}
       logger.error send_message
