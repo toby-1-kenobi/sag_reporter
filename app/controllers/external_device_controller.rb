fca8990d2a182be351faf66d0f68029bc6a150dc
Silas-Germany <silas@wieberneit.de>
2017-12-07 22:01:56 +0530

    Forgot comma

--- app/controllers/external_device_controller.rb
+++ app/controllers/external_device_controller.rb
@@ -215,6 +215,7 @@ class ExternalDeviceController < ApplicationController
                 data: image_data,
                 updated_at: uploaded_file.updated_at.to_i
             }.to_json)
+            pictures_data.write(', ') unless file.length == 0
           else
             errors << {"uploaded_file_#{uploaded_file.id}" => "permission denied"}
           end
