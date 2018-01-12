51b436cf606242b4079d2b2674999de641518c40
Toby Anderson <toby.anderson@gmail.com>
2018-01-12 23:04:08 +1100

    Create initial language table with just language names.

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -14,6 +14,7 @@ class ZonesController < ApplicationController
     @zone = Zone.find params[:id]
     redirect_to zones_path unless logged_in_user.national? or logged_in_user.zones.include? @zone
     @languages = Language.includes({geo_states: :zone}, :family, {finish_line_progresses: :finish_line_marker}).user_limited(logged_in_user).where(geo_states: {zone: @zone})
+    @flms = FinishLineMarker.order(:number)
     @geo_states = @zone.geo_states
     @geo_states = @geo_states.where(id: logged_in_user.geo_states) unless logged_in_user.national?
     @filters = {since: 3.month.ago.strftime('%d %B, %Y'), until: Date.today.strftime('%d %B, %Y')}
