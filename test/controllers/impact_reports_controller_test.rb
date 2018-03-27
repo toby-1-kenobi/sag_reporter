ea8e5390aa2c9d258d43c5fb96a03f7dcc4a210a
Toby Anderson <toby.anderson@gmail.com>
2018-03-27 18:32:22 +0530

    Allow low-sensitivity users who have made their own impact reports to tag those reports.
    (cherry picked from commit fce33ba)

--- test/controllers/impact_reports_controller_test.rb
+++ test/controllers/impact_reports_controller_test.rb
@@ -2,4 +2,18 @@ require "test_helper"
 
 describe ImpactReportsController do
 
+  it 'lets high sensitivity users see reports of others when tagging' do
+    log_in_as users(:andrew)
+    get :tag
+    _(response).must_be :success?
+    _(assigns(:reports).pluck :id).must_include impact_reports(:'impact-report-norman').id
+  end
+
+  it 'doesnt let low sensitivity users see reports of others when tagging' do
+    log_in_as users(:norman)
+    get :tag
+    _(response).must_be :success?
+    _(assigns(:reports).pluck :id).wont_include impact_reports(:'impact-report-andrew').id
+  end
+
 end
