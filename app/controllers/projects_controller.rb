ee7d2655cf42f43c1b3d9112045a3d4824d7daed
Toby Anderson <toby.anderson@gmail.com>
2018-04-04 22:34:25 +0530

    Enable adding new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,14 +2,27 @@ class ProjectsController < ApplicationController
 
   before_action :require_login
 
+  before_action only: [:create] do
+    head :forbidden unless logged_in_user.admin?
+  end
+
   def index
     redirect_to root_path unless logged_in_user.admin?
     @projects = Project.all
   end
 
   def create
+    @project = Project.create(project_params)
+    respond_to :js
   end
 
   def delete
   end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
 end
