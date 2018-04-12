bb4b58d7b4463efa1bacfe580d5a8f8fead28252
Toby Anderson <toby.anderson@gmail.com>
2018-04-12 15:42:42 +0530

    Implement the remained of the PB API.

--- app/controllers/pb_api_controller.rb
+++ app/controllers/pb_api_controller.rb
@@ -18,6 +18,28 @@ class PbApiController < ApplicationController
   end
 
   def language_details
+    render json: { status: 'fail', error: 'not authorised' } and return unless request.headers['Authorization']
+    token = request.headers['Authorization'].split.last
+    payload = {}
+    begin
+      payload = decode_jwt(token)
+    rescue JWT::VerificationError
+      render json: { status: 'fail', error: 'corrupt authorisation token' } and return
+    end
+    render json: { status: 'fail', error: 'wrong authorisation token' } and return unless payload['user_id']
+    render json: { status: 'fail', error: 'user not found' } and return unless User.find payload['user_id']
+    language = Language.find_by_iso params[:iso]
+    render json: { status: 'fail', error: 'language not found', iso: params[:iso] } and return unless language
+    language_details =  { iso: language.iso, name: language.name }
+    markers_of_interest = [5, 6, 7, 2, 1]
+    finish_line_progresses = language.finish_line_progresses.includes(:finish_line_marker).where( finish_line_markers: { number: markers_of_interest})
+    finish_line_progresses.each do |flp|
+      language_details[flp.finish_line_marker.name] = flp.status
+    end
+    render json: {
+        status: 'success',
+        language: language_details
+    }
   end
 
 end
