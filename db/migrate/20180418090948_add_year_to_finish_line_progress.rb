f25c2607e604d3c41d63a87d1ee35198391c2f75
Toby Anderson <toby.anderson@gmail.com>
2018-04-24 13:30:24 +0530

    Make the migration that adds year to finish line progress reversible.

--- db/migrate/20180418090948_add_year_to_finish_line_progress.rb
+++ db/migrate/20180418090948_add_year_to_finish_line_progress.rb
@@ -2,7 +2,7 @@ class AddYearToFinishLineProgress < ActiveRecord::Migration
   def change
     add_column :finish_line_progresses, :year, :integer,  :null => :true, :default => :null
 
-    remove_index :finish_line_progresses, name: 'index_lang_finish_line'
+    remove_index :finish_line_progresses, name: 'index_lang_finish_line', column: [:language_id, :finish_line_marker_id], unique: true
 
     add_index :finish_line_progresses, [:language_id, :finish_line_marker_id, :year], unique: true, name: 'index_lang_finish_line'
 
