e45d64c8034eb64c3d872666d43c5db91d59ed37
Toby Anderson <toby.anderson@gmail.com>
2018-05-14 17:28:58 +0530

    For filling out the languages table in the dashboard, don't create lots of finish line progresses, instead show the latest. Then create missing ones when they get edited.

--- test/models/finish_line_progress_test.rb
+++ test/models/finish_line_progress_test.rb
@@ -1,15 +1,85 @@
-require "test_helper"
+require 'test_helper'
 
 describe FinishLineProgress do
-  let(:finish_line_progress) { FinishLineProgress.new status: 0}
+  let(:flp) { FinishLineProgress.new(
+      status: 0,
+      language: languages(:toto),
+      finish_line_marker: finish_line_markers(:flm_00)
+  ) }
 
   it "must be valid" do
-    value(finish_line_progress).must_be :valid?
+    value(flp).must_be :valid?
   end
 
   it "wont be valid without status" do
-    finish_line_progress.status = nil
-    value(finish_line_progress).wont_be :valid?
+    flp.status = nil
+    value(flp).wont_be :valid?
+  end
+
+  it "wont be valid without language" do
+    flp.language = nil
+    value(flp).wont_be :valid?
+  end
+
+  it "wont be valid without marker" do
+    flp.finish_line_marker = nil
+    value(flp).wont_be :valid?
+  end
+
+  it 'gets the current year' do
+    # edge case: the day before our system ticks over to the next year
+    travel_to Time.zone.local(2025, 9, 30)
+    _(FinishLineProgress.get_current_year).must_equal 2025
+  end
+
+  it 'knows that the year ticks over on Oct 1' do
+    # edge case: the day after our system ticks over to the next year
+    travel_to Time.zone.local(2025, 10, 1)
+    _(FinishLineProgress.get_current_year).must_equal 2026
+  end
+
+  it "finds the current one if there's none closer earlier than specified year" do
+    attributes = flp.attributes.except('id', 'created_at', 'updated_at', 'year')
+    flp.save
+    FinishLineProgress.create(attributes.merge(year: 5000))
+    _(FinishLineProgress.closest_to(flp.language_id, flp.finish_line_marker_id, 3000)).must_equal flp
+  end
+
+  it "finds the latest one before specified year" do
+    attributes = flp.attributes.except('id', 'created_at', 'updated_at', 'year')
+    flp.save
+    FinishLineProgress.create(attributes.merge(year: 5000))
+    target = FinishLineProgress.create(attributes.merge(year: 2050))
+    FinishLineProgress.create(attributes.merge(year: 2020))
+    _(FinishLineProgress.closest_to(flp.language_id, flp.finish_line_marker_id, 3000)).must_equal target
+  end
+
+  it "doesn't create when it finds" do
+    flp.year = 2020
+    attributes = flp.attributes.except('id', 'created_at', 'updated_at')
+    flp.save
+    assert_no_difference('FinishLineProgress.count') do
+      FinishLineProgress.find_or_create_in_sequence(attributes)
+    end
+  end
+
+  it "creates from next year when it only finds current" do
+    attributes = flp.attributes.except('id', 'created_at', 'updated_at', 'year')
+    flp.save
+    FinishLineProgress.stub :get_current_year, 2018 do
+      assert_difference('FinishLineProgress.count', +2) do
+        FinishLineProgress.find_or_create_in_sequence(attributes.merge(year: 2020))
+      end
+    end
+  end
+
+  it "creates current year and from next year when it finds none" do
+    attributes = flp.attributes.except('id', 'created_at', 'updated_at', 'year')
+    FinishLineProgress.stub :get_current_year, 2018 do
+      assert_difference('FinishLineProgress.count', +3) do
+        FinishLineProgress.find_or_create_in_sequence(attributes.merge(year: 2020))
+      end
+    end
   end
 
 end
