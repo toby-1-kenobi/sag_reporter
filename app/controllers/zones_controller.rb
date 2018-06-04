115c4128404561b312fb3d269ca64222ecfe00a8
Toby Anderson <toby.anderson@gmail.com>
2018-06-04 17:52:23 +0530

    Fix population problem in LCI board report better.

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -98,7 +98,8 @@ class ZonesController < ApplicationController
       lang.finish_line_progresses.includes(:finish_line_marker).where(year: nil).each do |flp|
         lang_data[flp.finish_line_marker.name] = flp.status
       end
-      lang_data[:pop] = lang.best_current_pop.amount || 0
+      pop = lang.best_current_pop
+      lang_data[:pop] = pop ? pop.amount : 0
       @language_data << lang_data
     end
     respond_to do |format|
