9c33a9f94c75ecbbdeafe5326971fff8cc5f417d
Toby Anderson <toby.anderson@gmail.com>
2018-08-07 23:06:23 +0530

    Fix error in validation syntax.

--- app/models/church_congregation.rb
+++ app/models/church_congregation.rb
@@ -1,9 +1,11 @@
 class ChurchCongregation < ActiveRecord::Base
   belongs_to :organisation
-  belongs_to :village, uniqueness: { scope: :organisation }
+  belongs_to :village
   has_many :users, dependent: :nullify
   has_many :planned_ministries, dependent: :destroy
   has_many :actual_ministries, dependent: :destroy
+
+  validates :village, uniqueness: { scope: :organisation }
 
   def full_name
     description = "#{organisation ? organisation.name : 'independent church'} in #{village.name}"
