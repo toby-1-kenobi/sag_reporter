7b8aadf7583443d384c25f816e4c57b531c51826
Silas-Germany <silas.ne.india@gmail.com>
2018-09-17 20:34:52 +0200

    Creator Id was missing

--- app/controllers/android_sync_controller.rb
+++ app/controllers/android_sync_controller.rb
@@ -333,6 +333,7 @@ class AndroidSyncController < ApplicationController
               :creator,
               :value,
               :comment,
+              :creator_id,
               :actual
           ]
       ]
