89f1563f4e4265f996b1d66cea091e253a0ed499
Toby Anderson <toby.anderson@gmail.com>
2018-09-22 15:16:36 +0530

    Add calculation_method and reporter to deliverables.

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -1,4 +1,17 @@
 class Deliverable < ActiveRecord::Base
+
+  enum calculation_method: {
+      most_recent: 0,
+      sum_of_all: 1
+  }
+
+  enum reporter: {
+      church_team: 0,
+      facilitator: 1,
+      supervisor: 2,
+      auto: 3
+  }
+
   belongs_to :ministry
   has_many :ministry_outputs, dependent: :restrict_with_exception
   has_many :quarterly_targets, dependent: :restrict_with_error
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920052419) do
+ActiveRecord::Schema.define(version: 20180920105415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,10 +169,12 @@ ActiveRecord::Schema.define(version: 20180920052419) do
   add_index "data_sources", ["name"], name: "index_data_sources_on_name", unique: true, using: :btree
 
   create_table "deliverables", force: :cascade do |t|
-    t.integer  "ministry_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "number",      null: false
+    t.integer  "ministry_id",                    null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "number",                         null: false
+    t.integer  "calculation_method", default: 0, null: false
+    t.integer  "reporter",           default: 0, null: false
   end
 
   add_index "deliverables", ["ministry_id"], name: "index_deliverables_on_ministry_id", using: :btree
