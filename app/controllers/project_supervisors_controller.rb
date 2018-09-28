6f181737cf95091631f56cd3776886733b677d8b
Toby Anderson <toby.anderson@gmail.com>
2018-09-28 14:35:42 +0530

    Make project supervisor list update when adding or removing a supervisor. Enable a selectbox for supervisor role.

--- app/controllers/project_supervisors_controller.rb
+++ app/controllers/project_supervisors_controller.rb
@@ -5,14 +5,33 @@ class ProjectSupervisorsController < ApplicationController
     ps = ProjectSupervisor.create(project: @project, user_id: params[:supervisor], role: 0)
     Rails.logger.error("Failed to add supervisor to project: #{ps.errors.full_messages.to_sentence}") unless ps.persisted?
     respond_to do |format|
-      format.js { render :template => "projects/add_supervisor" }
+      format.js { render :template => "projects/refresh_supervisor_list" }
     end
   end
 
   def destroy
     ps = ProjectSupervisor.find(params[:id])
+    @project = ps.project
     ps.destroy
-    respond_to :js
+    respond_to do |format|
+      format.js { render :template => "projects/refresh_supervisor_list" }
+    end
   end
+
+  def update
+    ps = ProjectSupervisor.find(params[:id])
+    ps.update_attributes(project_supervisor_params)
+    @project = ps.project
+    respond_to do |format|
+      format.js { render :template => "projects/refresh_supervisor_list" }
+    end
+  end
+
+  private
+
+  def project_supervisor_params
+    params.require(:project_supervisor).permit([:role])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -144,7 +144,7 @@ Rails.application.routes.draw do
 
   patch 'project_streams/:id/set_supervisor/:supervisor', to: 'project_streams#set_supervisor', as: 'set_supervisor_in_project_stream'
 
-  resources :project_supervisors, only: [:create, :destroy]
+  resources :project_supervisors, only: [:create, :destroy, :update]
 
   resources :reports do
     collection do
