4227bd11b41b1b50037787285a540875abc8594f
Silas-Germany <silas.ne.india@gmail.com>
2018-10-19 19:41:19 +0200

    Some additional helpful methods for working with the translation table

--- app/models/translation_code.rb
+++ app/models/translation_code.rb
@@ -2,9 +2,25 @@ class TranslationCode < ActiveRecord::Base
   has_many :translations, dependent: :destroy
 
   def connected_to
-    Ministry.find_by(name: id) ||
+    Ministry.find_by(name_id: id) ||
         Deliverable.find_by(short_form_id: id) ||
         Deliverable.find_by(plan_form_id: id) ||
         Deliverable.find_by(result_form_id: id)
   end
-end
+  
+  def en
+    translations.find{|t| t.language_id == 1 }&.content
+  end
+
+  def hi
+    translations.find{|t| t.language_id == 2 }&.content
+  end
+  
+  def en=(value)
+    (translations.find{|t| t.language_id == 1 } || Translation.create(language_id:1)).update content: value
+  end
+
+  def hi=(value)
+    (translations.find{|t| t.language_id == 2 } || Translation.create(language_id: 2)).update content: value
+  end
+end 
