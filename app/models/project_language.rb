1f77eeb2b827b19ba64278d3d3006e57bd91c228
Toby Anderson <toby.anderson@gmail.com>
2018-10-25 11:04:04 +1100

    Enable touching of project when project languages are changed.

--- app/models/project_language.rb
+++ app/models/project_language.rb
@@ -1,6 +1,21 @@
 class ProjectLanguage < ActiveRecord::Base
+
   belongs_to :project
   belongs_to :state_language
+
   validates :project, presence: true
   validates :state_language, presence: true, uniqueness: { scope: :project }
+
+  after_create do |project_language|
+    project_language.project.touch
+  end
+
+  after_destroy do |project_language|
+    project_language.project.touch
+  end
+
+  after_update do |project_language|
+    project_language.project.touch
+  end
+
 end
--- test/models/project_language_test.rb
+++ test/models/project_language_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe ProjectLanguage do
+
   let(:project_language) { FactoryBot.build(:project_language) }
 
   it "must be valid" do
     value(project_language).must_be :valid?
   end
+
+  it "must update project when a new language is added" do
+    project = FactoryBot.create(:project, updated_at: 1.day.ago)
+    init_value = project.updated_at
+    FactoryBot.create(:project_language, project: project)
+    project.reload
+    _(project.updated_at).must_be :>, init_value
+  end
+
+  it "must update project when a language is removed" do
+    project = FactoryBot.create(:project)
+    project_language = FactoryBot.create(:project_language, project: project)
+    init_value = project.updated_at
+    project_language.destroy
+    project.reload
+    _(project.updated_at).must_be :>, init_value
+  end
+
+  it "must update project when a language is changed" do
+    project = FactoryBot.create(:project)
+    project_language = FactoryBot.create(:project_language, project: project)
+    init_value = project.updated_at
+    project_language.state_language = FactoryBot.create(:state_language)
+    project_language.save
+    project.reload
+    _(project.updated_at).must_be :>, init_value
+  end
+
 end
