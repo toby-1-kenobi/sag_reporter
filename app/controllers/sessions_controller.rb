2a988c52c9f252e53b39185a1d12ce24c3d1f745
Toby Anderson <toby.anderson@gmail.com>
2019-03-14 12:24:18 +1100

    remove authentication for sandbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -39,8 +39,8 @@ class SessionsController < ApplicationController
         # otherwise we'll assume it's their phone number
         @user = User.find_by(phone: username)
       end
-      # skip authentication for the development environment
-      if @user and Rails.env.development?
+      # skip authentication for the development environment and sandbox
+      if @user and (Rails.env.development? or ENV['REV79_VARIETY'].downcase == 'sandbox')
         log_in @user
         remember @user
         redirect_back_or root_path and return
