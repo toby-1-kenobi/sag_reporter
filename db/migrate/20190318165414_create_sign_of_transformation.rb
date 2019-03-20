45da2c56214ffbaf126bf9ab3eb22ec2275f12e2
Silas Wieberneit <silas.ne.india@gmail.com>
2019-03-20 10:58:42 +0100

    One more change for the new created table

--- db/migrate/20190318165414_create_sign_of_transformation.rb
+++ db/migrate/20190318165414_create_sign_of_transformation.rb
@@ -3,7 +3,6 @@ class CreateSignOfTransformation < ActiveRecord::Migration
     create_table :sign_of_transformations do |t|
       t.references :church_ministry, index: true, foreign_key: true, null: false
       t.string :month, null: false
-      t.boolean :actual, null: false
       t.references :progress_marker, index: true, foreign_key: true
       t.string :other
 
