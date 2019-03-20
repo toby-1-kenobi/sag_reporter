269dec82bb7bae28d3902e04b43c8c1b11583512
Toby Anderson <toby.anderson@gmail.com>
2019-03-21 10:36:59 +1100

    Track verse counts for OT and NT in translation progress interface

--- app/controllers/translation_progresses_controller.rb
+++ app/controllers/translation_progresses_controller.rb
@@ -4,12 +4,17 @@ class TranslationProgressesController < ApplicationController
 
   def create
     @translation_progress = TranslationProgress.create(translation_progress_params)
+    count_verses(@translation_progress.language_id, @translation_progress.deliverable_id)
     respond_to :js
   end
 
   def destroy
     @translation_progress_id = params[:id]
-    TranslationProgress.find(@translation_progress_id).destroy
+    tp = TranslationProgress.find(@translation_progress_id)
+    @lang_id = tp.language_id
+    deliverable_id = tp.deliverable_id
+    tp.destroy
+    count_verses(@lang_id, deliverable_id)
     respond_to :js
   end
 
@@ -17,12 +22,23 @@ class TranslationProgressesController < ApplicationController
     @lang_id = params[:language]
     @deliverable_id = params[:deliverable]
     @lang_stream_id = params[:lang_stream]
-    # @this_month = TranslationProgress.where(language_id: @lang_id, deliverable_id: @deliverable_id, month: month).pluck :chapter_id
-    # @other_month = TranslationProgress.where(language_id: @lang_id, deliverable_id: @deliverable_id).where.not(month: month).pluck :chapter_id
+    count_verses(@lang_id, @deliverable_id)
     respond_to :js
   end
 
   private
+
+  def count_verses(language_id, deliverable_id)
+    @counts = {ot: {}, nt: {}}
+    @counts.keys.each do |testament|
+      @counts[testament] = {by_month: Hash.new(0)}
+      verse_totals = Chapter.joins(:book).where(books: {nt: testament == :nt}).pluck(:id, :verses).to_h
+      completed_chapters = TranslationProgress.joins(chapter: :book).where(language_id: language_id, deliverable_id: deliverable_id, books: {nt: testament == :nt}).pluck(:chapter_id, :month).to_h
+      completed_chapters.each{ |chapter, month| @counts[testament][:by_month][month || 'none'] += verse_totals[chapter] }
+      @counts[testament][:total_verses] = verse_totals.select{ |chapter, _| completed_chapters.keys.include? chapter }.reduce(0){ |sum, vt| sum + vt[1] }
+      @counts[testament][:remaining_verses] = verse_totals.select{ |chapter, _| completed_chapters.keys.exclude? chapter }.reduce(0){ |sum, vt| sum + vt[1] }
+    end
+  end
 
   def translation_progress_params
     params.require(:translation_progress).permit([
