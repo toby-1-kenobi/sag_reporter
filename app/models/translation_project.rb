9a3281e37690446be1cabe155fb7d39b756fd126
Toby Anderson <toby.anderson@gmail.com>
2019-03-26 14:34:11 +1100

    Add a table of completed verses at the bottom of the language translation tab.

--- app/models/translation_project.rb
+++ app/models/translation_project.rb
@@ -1,6 +1,7 @@
 class TranslationProject < ActiveRecord::Base
   belongs_to :language
   belongs_to :project
+  has_many :translation_progresses, dependent: :destroy
   validates :language_id, presence: true
   validates :project_id, presence: true, uniqueness: {scope: :language_id}
 end
