78a48bfbe1b919513c87a321a53f71a4b43a00bc
Toby Anderson <toby.anderson@gmail.com>
2019-04-02 23:06:35 +1100

    increase frequency of puma restarts to every 3 hours

--- config/puma.rb
+++ config/puma.rb
@@ -18,5 +18,5 @@ end
 before_fork do
   require 'puma_worker_killer'
 
-  PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
+  PumaWorkerKiller.enable_rolling_restart(3 * 3600) # 3 hours in seconds
 end
